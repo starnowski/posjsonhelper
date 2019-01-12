8a2bfe6b9ee6923b6f48c8b1fbba1ca9864c5247
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-01-12 15:06:28 +0100

    #3 - Added "Employee" entity to test numeric values.

--- poc/src/main/resources/com/github/starnowski/posjsonhelper/poc/schema-postgresql.sql
+++ poc/src/main/resources/com/github/starnowski/posjsonhelper/poc/schema-postgresql.sql
@@ -1,3 +1,8 @@
 CREATE OR REPLACE FUNCTION jsonb_all_array_strings_exist(x jsonb, y text[]) RETURNS boolean AS $$
     SELECT x ?& y;
 $$ LANGUAGE SQL;
+
+
+CREATE OR REPLACE FUNCTION jsonb_top_level_exist(x jsonb, y text) RETURNS boolean AS $$
+    SELECT x ? y;
+$$ LANGUAGE SQL;
--- poc/src/test/resources/com/github/starnowski/posjsonhelper/poc/books-with-multi-authors.sql
+++ poc/src/test/resources/com/github/starnowski/posjsonhelper/poc/books-with-multi-authors.sql
@@ -2,3 +2,4 @@
 INSERT INTO book_reader (id, jsonb_data) VALUES (1, '{"favourite_authors": [ "William Shakespeare"] }');
 INSERT INTO book_reader (id, jsonb_data) VALUES (2, '{"favourite_authors": [ "William Shakespeare", "Stephen King"] }');
 INSERT INTO book_reader (id, jsonb_data) VALUES (3, '{"favourite_authors": [ "William Shakespeare", "Isaac Asimov", "Stephen King"] }');
+
