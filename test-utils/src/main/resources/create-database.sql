6633f0036c39c98b88d1b7673ed30834a7d4bb54
starnowski <33316705+starnowski@users.noreply.github.com>
2022-08-18 01:47:14 +0200

    #6 Added CI action and database scripts

---
+++ test-utils/src/main/resources/create-database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE posjsonhelper_db WITH
+  OWNER = "posjsonhelper-owner"
+ENCODING = 'UTF8'
+CONNECTION LIMIT = -1;
+
+GRANT ALL PRIVILEGES ON DATABASE posjsonhelper_db to "posjsonhelper-owner" ;
