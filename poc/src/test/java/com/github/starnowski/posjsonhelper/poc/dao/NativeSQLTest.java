19e6cb9aa6836996e232215061a082e6c6dd9823
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-08-22 01:14:57 +0200

    #6 - Added test [skip ci]

---
+++ poc/src/test/java/com/github/starnowski/posjsonhelper/poc/dao/NativeSQLTest.java
@@ -0,0 +1,23 @@
+package com.github.starnowski.posjsonhelper.poc.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.github.starnowski.posjsonhelper.poc.TestUtils.CLEAR_DATABASE_SCRIPT_PATH;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(value = CLEAR_DATABASE_SCRIPT_PATH,
+        config = @SqlConfig(transactionMode = ISOLATED),
+        executionPhase = BEFORE_TEST_METHOD)
+@Sql(value = CLEAR_DATABASE_SCRIPT_PATH,
+        config = @SqlConfig(transactionMode = ISOLATED),
+        executionPhase = AFTER_TEST_METHOD)
+public class NativeSQLTest {
+}
---
+++ poc/src/test/resources/com/github/starnowski/posjsonhelper/poc/items.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO item (id, jsonb_content) VALUES (1, '{"top_element_with_set_of_values":["TAG1","TAG2","TAG11","TAG12"]}');
+INSERT INTO item (id, jsonb_content) VALUES (2, '{"top_element_with_set_of_values":["TAG3"]}');
+INSERT INTO item (id, jsonb_content) VALUES (3, '{"top_element_with_set_of_values":["TAG1","TAG3"]}');
