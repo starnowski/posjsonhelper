3fdad485aad64de2f76be1fd462b56191a6fc77a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-08-24 00:35:19 +0200

    #6 - Added groovy tests case [skip ci]

--- poc/src/test/resources/com/github/starnowski/posjsonhelper/poc/items.sql
+++ poc/src/test/resources/com/github/starnowski/posjsonhelper/poc/items.sql
@@ -1,4 +1,5 @@
 
-INSERT INTO item (id, jsonb_content) VALUES (1, '{"top_element_with_set_of_values":["TAG1","TAG2","TAG11","TAG12"]}');
+INSERT INTO item (id, jsonb_content) VALUES (1, '{"top_element_with_set_of_values":["TAG1","TAG2","TAG11","TAG12","TAG21","TAG22"]}');
 INSERT INTO item (id, jsonb_content) VALUES (2, '{"top_element_with_set_of_values":["TAG3"]}');
 INSERT INTO item (id, jsonb_content) VALUES (3, '{"top_element_with_set_of_values":["TAG1","TAG3"]}');
+INSERT INTO item (id, jsonb_content) VALUES (4, '{"top_element_with_set_of_values":["TAG22","TAG21"]}');
