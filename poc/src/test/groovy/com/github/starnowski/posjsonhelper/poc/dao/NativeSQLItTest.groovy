788fbe994b16be5db707d5ce204e841910b55d5a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-08-23 23:45:35 +0200

    #6 - Added groovy tests case [skip ci]

--- poc/src/test/groovy/com/github/starnowski/posjsonhelper/poc/dao/NativeSQLItTest.groovy
+++ poc/src/test/groovy/com/github/starnowski/posjsonhelper/poc/dao/NativeSQLItTest.groovy
@@ -9,6 +9,7 @@ import org.springframework.test.context.jdbc.SqlGroup
 import spock.lang.Unroll
 
 import javax.persistence.EntityManager
+import java.util.stream.Collectors
 
 import static com.github.starnowski.posjsonhelper.poc.TestUtils.CLEAR_DATABASE_SCRIPT_PATH
 import static com.github.starnowski.posjsonhelper.poc.TestUtils.ITEMS_SCRIPT_PATH
@@ -30,27 +31,23 @@ class NativeSQLItTest extends spock.lang.Specification {
     @Autowired
     EntityManager entityManager
 
-//    @Test
-//    @Sql(value = {CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH},
-//            config = @SqlConfig(transactionMode = ISOLATED),
-//            executionPhase = BEFORE_TEST_METHOD)
-//    public void shouldReturnCorrectNumberOrResults() throws SQLException {
-//        // when
-//        Long result = TestUtils.selectAndReturnFirstRecordAsLong(entityManager, "SELECT id FROM item WHERE jsonb_all_array_strings_exist(jsonb_extract_path(jsonb_content, 'top_element_with_set_of_values'), array['TAG1', 'TAG2']) ");
-//
-//        // then
-//        Assertions.assertThat(result).isEqualTo(1);
-//    }
-
     @Unroll
     @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
             config = @SqlConfig(transactionMode = ISOLATED),
             executionPhase = BEFORE_TEST_METHOD)
-    def "should return correct when searching by tags" () {
+    def "should return single correct when searching by tags" () {
+        given:
+            def pattern = "SELECT id FROM item WHERE jsonb_all_array_strings_exist(jsonb_extract_path(jsonb_content, 'top_element_with_set_of_values'), array[%s]) "
+            def query = String.format(pattern, tags.stream().map({it -> return "'" + it + "'"}).collect(Collectors.joining(", ")))
+
         when:
-            Long result = TestUtils.selectAndReturnFirstRecordAsLong(entityManager, "SELECT id FROM item WHERE jsonb_all_array_strings_exist(jsonb_extract_path(jsonb_content, 'top_element_with_set_of_values'), array['TAG1', 'TAG2']) ");
+            Long result = TestUtils.selectAndReturnFirstRecordAsLong(entityManager, query);
 
         then:
             result == 1
+
+        where:
+            tags    ||  expectedId
+            ['TAG1', 'TAG2']      ||  1
     }
 }
