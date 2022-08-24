735294c7e20939ec35b4a6a9cf019f08acc02c1a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-08-24 02:02:27 +0200

    #6 - Added tests cases [skip ci]

--- poc/src/test/groovy/com/github/starnowski/posjsonhelper/poc/dao/NativeSQLItTest.groovy
+++ poc/src/test/groovy/com/github/starnowski/posjsonhelper/poc/dao/NativeSQLItTest.groovy
@@ -96,4 +96,26 @@ class NativeSQLItTest extends spock.lang.Specification {
             ['TAG3']                        ||  [3, 2].toSet()
             ['TAG1', 'TAG32']               ||  [1, 3, 5].toSet()
     }
+
+//    @Unroll
+//    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
+//            config = @SqlConfig(transactionMode = ISOLATED),
+//            executionPhase = BEFORE_TEST_METHOD)
+//    def "should return correct id #expectedIds when searching by any matching tags [#tags]" () {
+//        given:
+//        def pattern = "SELECT id FROM item WHERE jsonb_any_array_strings_exist(jsonb_extract_path(jsonb_content, 'top_element_with_set_of_values'), array[%s]) "
+//        def query = String.format(pattern, tags.stream().map({it -> return "'" + it + "'"}).collect(Collectors.joining(", ")))
+//
+//        when:
+//        def result = TestUtils.selectAndReturnSetOfLongObjects(entityManager, query);
+//
+//        then:
+//        result == expectedIds
+//
+//        where:
+//        tags                            ||  expectedIds
+//        ['TAG1', 'TAG2']                ||  [1, 3].toSet()
+//        ['TAG3']                        ||  [3, 2].toSet()
+//        ['TAG1', 'TAG32']               ||  [1, 3, 5].toSet()
+//    }
 }
--- poc/src/test/resources/com/github/starnowski/posjsonhelper/poc/items.sql
+++ poc/src/test/resources/com/github/starnowski/posjsonhelper/poc/items.sql
@@ -4,3 +4,11 @@ INSERT INTO item (id, jsonb_content) VALUES (2, '{"top_element_with_set_of_value
 INSERT INTO item (id, jsonb_content) VALUES (3, '{"top_element_with_set_of_values":["TAG1","TAG3"]}');
 INSERT INTO item (id, jsonb_content) VALUES (4, '{"top_element_with_set_of_values":["TAG22","TAG21"]}');
 INSERT INTO item (id, jsonb_content) VALUES (5, '{"top_element_with_set_of_values":["TAG31","TAG32"]}');
+
+-- item without any properties, just an empty json
+INSERT INTO item (id, jsonb_content) VALUES (6, '{}');
+
+
+INSERT INTO item (id, jsonb_content) VALUES (7, '{"integer_value": 132}');
+INSERT INTO item (id, jsonb_content) VALUES (8, '{"integer_value": 562}');
+INSERT INTO item (id, jsonb_content) VALUES (9, '{"integer_value": 1322}');
