0e5635e768981fae505c495ad9a94d3559e6695f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-08-27 17:09:27 +0200

    #6 - Added demo types [skip ci]

---
+++ hibernate5/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate5/demo/dao/ItemDaoItTest.groovy
@@ -0,0 +1,6 @@
+package com.github.starnowski.posjsonhelper.hibernate5.demo.dao
+
+import spock.lang.Specification
+
+class ItemDaoItTest extends Specification {
+}
---
+++ hibernate5/src/test/java/com/github/starnowski/posjsonhelper/hibernate5/demo/dao/ItemDao.java
@@ -0,0 +1,7 @@
+package com.github.starnowski.posjsonhelper.hibernate5.demo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemDao {
+}
