baeabc8d15f737f34d0bf96ff2d782c009706eab
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-09-29 00:38:00 +0200

    #10 - Added script that creates non-public schema

---
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/sql/functions/JsonbAllArrayStringsExistFunctionProducer.java
@@ -0,0 +1,8 @@
+package com.github.starnowski.posjsonhelper.core.sql.functions;
+
+public class JsonbAllArrayStringsExistFunctionProducer extends AbstractDefaultFunctionDefinitionFactory{
+    @Override
+    protected String produceStatement(DefaultFunctionFactoryParameters parameters) {
+        return null;
+    }
+}
---
+++ test-utils/src/main/resources/create-database-non-public-schema.sql
@@ -0,0 +1,2 @@
+-- The separate database schema, used for the test cases where there are no default privileges set, just like in case of 'public' schema.
+CREATE SCHEMA non_public_schema AUTHORIZATION "posjsonhelper-owner";
