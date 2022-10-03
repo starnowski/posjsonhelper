a324b3fbe72c94149d42b549e065afa868dfaa9d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-04 01:48:32 +0200

    #10 - Fixed GitHub action CI

--- test-utils/src/main/resources/create-database-non-public-schema.sql
+++ test-utils/src/main/resources/create-database-non-public-schema.sql
@@ -1,2 +1,4 @@
+
+\c posjsonhelper_db;
 -- The separate database schema, used for the test cases where there are no default privileges set, just like in case of 'public' schema.
 CREATE SCHEMA non_public_schema AUTHORIZATION "posjsonhelper-owner";
