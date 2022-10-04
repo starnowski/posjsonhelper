626335be3df485a5ac10302333bf71ffec0d3112
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-04 09:20:27 +0200

    #10 - Fixed GitHub action CI, added additional database for only core module

--- test-utils/src/main/resources/create-database-non-public-schema.sql
+++
@@ -1,4 +0,0 @@
-
-\c posjsonhelper_db;
--- The separate database schema, used for the test cases where there are no default privileges set, just like in case of 'public' schema.
-CREATE SCHEMA non_public_schema AUTHORIZATION "posjsonhelper-owner";
