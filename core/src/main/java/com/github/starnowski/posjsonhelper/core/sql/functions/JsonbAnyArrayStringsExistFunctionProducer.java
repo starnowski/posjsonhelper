a19dd5e08a7f06d177cc53f0587838a2abea8b94
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-05 19:10:30 +0200

    #10 - Added producer for function jsonb_any_array_strings_exist [skip ci]

---
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/sql/functions/JsonbAnyArrayStringsExistFunctionProducer.java
@@ -0,0 +1,24 @@
+package com.github.starnowski.posjsonhelper.core.sql.functions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.github.starnowski.posjsonhelper.core.sql.functions.DefaultFunctionArgument.ofType;
+
+public class JsonbAnyArrayStringsExistFunctionProducer extends AbstractDefaultFunctionDefinitionFactory{
+
+    @Override
+    protected List<IFunctionArgument> prepareFunctionArguments(DefaultFunctionFactoryParameters parameters) {
+        return Arrays.asList(ofType("jsonb"), ofType("text[]"));
+    }
+
+    @Override
+    protected String buildBody(DefaultFunctionFactoryParameters parameters) {
+        return "SELECT $1 ?| $2;";
+    }
+
+    @Override
+    protected String prepareReturnType(DefaultFunctionFactoryParameters parameters) {
+        return "boolean";
+    }
+}
