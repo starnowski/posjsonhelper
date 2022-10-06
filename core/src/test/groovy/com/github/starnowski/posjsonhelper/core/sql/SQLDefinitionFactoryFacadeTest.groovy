e7dc4a1cf7df867498ec039ad1fb17d5c0728aba
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-06 23:49:09 +0200

    #10 - Added tests files [skip ci]

---
+++ core/src/test/groovy/com/github/starnowski/posjsonhelper/core/sql/SQLDefinitionFactoryFacadeTest.groovy
@@ -0,0 +1,40 @@
+package com.github.starnowski.posjsonhelper.core.sql
+
+import com.github.starnowski.posjsonhelper.core.Context
+import org.mockito.Mockito
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static java.util.stream.Collectors.toList
+import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.when
+
+class SQLDefinitionFactoryFacadeTest extends Specification {
+    //TODO
+
+    @Unroll
+    def "should return expected definitions with creation scripts (#creationScripts)"(){
+        given:
+            def context = Mock(Context)
+            def tested = new SQLDefinitionFactoryFacade(factories)
+
+        when:
+            def results = tested.build(context)
+
+        then:
+            results.stream().map({it -> it.getCreateScript()}).collect(toList()) == creationScripts
+
+        where:
+            factories   |   creationScripts
+            [mockSQLDefinitionContextFactoryWithCreationScript("Script1")]  |   ["Script1"]
+    }
+
+    private static ISQLDefinitionContextFactory mockSQLDefinitionContextFactoryWithCreationScript(String creationScript)
+    {
+        ISQLDefinitionContextFactory factory = mock(ISQLDefinitionContextFactory.class)
+        ISQLDefinition definition = mock(ISQLDefinition.class)
+        when(definition.getCreateScript()).thenReturn(creationScript)
+        when(factory.build(Mockito.any())).thenReturn(definition)
+        factory
+    }
+}
