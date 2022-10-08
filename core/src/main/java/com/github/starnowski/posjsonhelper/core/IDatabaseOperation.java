48404ea0f2e9a52b1f028546b52afcb1f220c05b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-08 21:35:32 +0200

    #10 - Added interface

---
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/IDatabaseOperation.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.posjsonhelper.core;
+
+import com.github.starnowski.posjsonhelper.core.sql.ISQLDefinition;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public interface IDatabaseOperation {
+
+    void run(DataSource dataSource, List<ISQLDefinition> sqlDefinitions);
+}
