be6c360a2ce0355ec4b5fcdc79ca6d351a0b3dd4
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-11 01:02:27 +0200

    #10 - Added implementation (not yet finished) [skip ci]

--- core/src/main/java/com/github/starnowski/posjsonhelper/core/operations/IDatabaseOperationsProcessor.java
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/operations/IDatabaseOperationsProcessor.java
@@ -3,9 +3,10 @@ package com.github.starnowski.posjsonhelper.core.operations;
 import com.github.starnowski.posjsonhelper.core.sql.ISQLDefinition;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IDatabaseOperationsProcessor {
 
-    void run(DataSource dataSource, List<ISQLDefinition> sqlDefinitions);
+    void run(DataSource dataSource, List<ISQLDefinition> sqlDefinitions) throws SQLException;
 }
--- core/src/main/java/com/github/starnowski/posjsonhelper/core/operations/ValidateOperationsProcessor.java
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/operations/ValidateOperationsProcessor.java
@@ -3,11 +3,28 @@ package com.github.starnowski.posjsonhelper.core.operations;
 import com.github.starnowski.posjsonhelper.core.sql.ISQLDefinition;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ValidateOperationsProcessor implements IDatabaseOperationsProcessor {
     @Override
-    public void run(DataSource dataSource, List<ISQLDefinition> sqlDefinitions) {
-
+    public void run(DataSource dataSource, List<ISQLDefinition> sqlDefinitions) throws SQLException {
+        Map<String, List<String>> failedChecks = new HashMap<>();
+        try (Connection connection = dataSource.getConnection()) {
+            //TODO
+//            connection.prepareStatement()
+//            sqlDefinitions.stream().map(selectStatement -> {
+//                        Long result = jdbcTemplate.execute((StatementCallback<Long>) statement -> {
+//                            ResultSet rs = statement.executeQuery(selectStatement);
+//                            rs.next();
+//                            return rs.getLong(1);
+//                        });
+//                        return new StatementAndItLongResult(selectStatement, result);
+//                    }
+//            ).collect(Collectors.toMap(sr -> sr.statement, sr -> sr.result));
+        }
     }
 }
