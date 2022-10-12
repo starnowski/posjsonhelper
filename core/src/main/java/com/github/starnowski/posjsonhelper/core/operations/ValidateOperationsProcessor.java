d6519f2083d8285b9c87629d9fee827167475726
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-13 00:53:13 +0200

    #10 - Added tests (not yet finished) [skip ci]

--- core/src/main/java/com/github/starnowski/posjsonhelper/core/operations/ValidateOperationsProcessor.java
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/operations/ValidateOperationsProcessor.java
@@ -1,18 +1,28 @@
 package com.github.starnowski.posjsonhelper.core.operations;
 
+import com.github.starnowski.posjsonhelper.core.operations.util.SQLUtil;
 import com.github.starnowski.posjsonhelper.core.sql.ISQLDefinition;
 import com.github.starnowski.posjsonhelper.core.util.Pair;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ValidateOperationsProcessor implements IDatabaseOperationsProcessor {
+
+    private final SQLUtil sqlUtil;
+
+    public ValidateOperationsProcessor() {
+        this(new SQLUtil());
+    }
+
+    ValidateOperationsProcessor(SQLUtil sqlUtil) {
+        this.sqlUtil = sqlUtil;
+    }
+
     @Override
     public void run(DataSource dataSource, List<ISQLDefinition> sqlDefinitions) throws SQLException {
         Map<String, List<String>> failedChecks = new HashMap<>();
@@ -20,19 +30,20 @@ public class ValidateOperationsProcessor implements IDatabaseOperationsProcessor
             //TODO
             sqlDefinitions.stream().flatMap(definition -> definition.getCheckingStatements().stream().map(cs -> new Pair<>(definition.getCreateScript(), cs)))
                     .filter(csKey -> {
-                        try {
-                            PreparedStatement statement = connection.prepareStatement(csKey.getValue());
-                            ResultSet rs = statement.executeQuery();
-                            rs.next();
-                            long result = rs.getLong(1);
-                            return result <= 0;
-                        } catch (SQLException e) {
-                            //TODO
-                            throw new RuntimeException(e);
-                        }
-                    }
-            );
+                                try {
+                                    long result = sqlUtil.returnLongResultForQuery(connection, csKey.getValue());
+                                    return result <= 0;
+                                } catch (SQLException e) {
+                                    //TODO
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                    );
 //                    .collect(Collectors.toMap(sr -> sr.statement, sr -> sr.result));
         }
+    }
+
+    SQLUtil getSqlUtil() {
+        return sqlUtil;
     }
 }
--- core/src/test/groovy/com/github/starnowski/posjsonhelper/core/operations/ValidateOperationsProcessorTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posjsonhelper/core/operations/ValidateOperationsProcessorTest.groovy
@@ -1,6 +1,18 @@
 package com.github.starnowski.posjsonhelper.core.operations
 
+import com.github.starnowski.posjsonhelper.core.operations.util.SQLUtil
 import spock.lang.Specification
 
 class ValidateOperationsProcessorTest extends Specification {
+
+    def "should return default instance type"(){
+        given:
+            def tested = new ValidateOperationsProcessor()
+
+        when:
+            def result = tested.getSqlUtil()
+
+        then:
+        result.getClass() == SQLUtil.class
+    }
 }
