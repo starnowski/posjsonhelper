87f6ed4b82d0e57936f5d954707d28c24104c374
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-19 19:15:32 +0200

    #10 - Added tests [skip ci]

--- core/src/test/groovy/com/github/starnowski/posjsonhelper/core/operations/CreateOperationsProcessorTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posjsonhelper/core/operations/CreateOperationsProcessorTest.groovy
@@ -1,6 +1,5 @@
 package com.github.starnowski.posjsonhelper.core.operations
 
-
 import com.github.starnowski.posjsonhelper.core.sql.ISQLDefinition
 import org.mockito.Mockito
 import spock.lang.Specification
@@ -8,6 +7,7 @@ import spock.lang.Unroll
 
 import javax.sql.DataSource
 import java.sql.Connection
+import java.sql.SQLException
 import java.sql.Statement
 
 class CreateOperationsProcessorTest extends Specification {
@@ -34,9 +34,35 @@ class CreateOperationsProcessorTest extends Specification {
             definitions << [[sqlDef("cre1"), sqlDef("cre2")], [sqlDef("creX"), sqlDef("creY"), sqlDef("creZ")]]
     }
 
+    @Unroll
+    def "should rethrow exception thrown during executing creation scripts"(){
+        given:
+            def tested = new CreateOperationsProcessor()
+            def dataSource = Mock(DataSource)
+            def connection = Mock(Connection)
+            def statement = Mock(Statement)
+            dataSource.getConnection() >> connection
+            connection.createStatement() >> statement
+            csThatThrowsExceptions.forEach({it ->
+                statement.execute(it) >> { throw new SQLException()}
+            })
+
+        when:
+            tested.run(dataSource, definitions)
+
+        then:
+            thrown(SQLException)
+
+        where:
+            definitions                                         |   csThatThrowsExceptions
+            [sqlDef("cre1"), sqlDef("cre2")]                    |   ["cre1"]
+            [sqlDef("creX"), sqlDef("creY"), sqlDef("creZ")]    |   ["creZ"]
+    }
+
     private static ISQLDefinition sqlDef(String createScript){
         ISQLDefinition definition = Mockito.mock(ISQLDefinition)
         Mockito.when(definition.getCreateScript()).thenReturn(createScript)
         definition
     }
+
 }
