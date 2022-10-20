b024cebc5593c7ea93f2bd62526c897a0959392d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-20 19:16:05 +0200

    #10 - Added tests [skip ci]

--- core/src/test/groovy/com/github/starnowski/posjsonhelper/core/DatabaseOperationExecutorFacadeTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posjsonhelper/core/DatabaseOperationExecutorFacadeTest.groovy
@@ -1,17 +1,16 @@
 package com.github.starnowski.posjsonhelper.core
 
 
+import com.github.starnowski.posjsonhelper.core.operations.exceptions.ValidationDatabaseOperationsException
 import com.github.starnowski.posjsonhelper.core.sql.ISQLDefinition
 import com.github.starnowski.posjsonhelper.core.sql.SQLDefinitionFactoryFacade
 import spock.lang.Specification
 import spock.lang.Unroll
 
 import javax.sql.DataSource
+import java.sql.SQLException
 
-import static com.github.starnowski.posjsonhelper.core.DatabaseOperationType.CREATE
-import static com.github.starnowski.posjsonhelper.core.DatabaseOperationType.DROP
-import static com.github.starnowski.posjsonhelper.core.DatabaseOperationType.LOG_ALL
-import static com.github.starnowski.posjsonhelper.core.DatabaseOperationType.VALIDATE
+import static com.github.starnowski.posjsonhelper.core.DatabaseOperationType.*
 
 class DatabaseOperationExecutorFacadeTest extends Specification {
 
@@ -36,5 +35,30 @@ class DatabaseOperationExecutorFacadeTest extends Specification {
 
         where:
             databaseOperation   << [CREATE, LOG_ALL, DROP, VALIDATE]
+    }
+
+    @Unroll
+    def"should rethrow exception [#exception] thrown by database operation processor"()
+    {
+        given:
+            def databaseOperationExecutor = Mock(DatabaseOperationExecutor)
+            def sqlDefinitionFactoryFacade = Mock(SQLDefinitionFactoryFacade)
+            def tested = new DatabaseOperationExecutorFacade(databaseOperationExecutor, sqlDefinitionFactoryFacade)
+            DataSource dataSource = Mock(DataSource)
+            Context context = Mock(Context)
+            List<ISQLDefinition> sqlDefinitions = Mock(List)
+
+
+        when:
+            tested.execute(dataSource, context, CREATE)
+
+        then:
+            1 * sqlDefinitionFactoryFacade.build(context) >> sqlDefinitions
+            1 * databaseOperationExecutor.execute(dataSource, sqlDefinitions, CREATE) >> { throw exception }
+            def ex = thrown(exception.getClass())
+            ex.is(exception)
+
+        where:
+            exception << [new SQLException(), new ValidationDatabaseOperationsException(new HashMap<String, Set<String>>())]
     }
 }
