d5d3b7c8bb0a8171207e012b1259c78a5c000261
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-20 19:23:33 +0200

    #10 - Added integration tests

--- hibernate5/src/test/java/com/github/starnowski/posjsonhelper/hibernate5/demo/SQLFunctionsConfiguration.java
+++ hibernate5/src/test/java/com/github/starnowski/posjsonhelper/hibernate5/demo/SQLFunctionsConfiguration.java
@@ -1,17 +1,14 @@
 package com.github.starnowski.posjsonhelper.hibernate5.demo;
 
 import com.github.starnowski.posjsonhelper.core.Context;
-import com.github.starnowski.posjsonhelper.core.DatabaseOperationExecutor;
+import com.github.starnowski.posjsonhelper.core.DatabaseOperationExecutorFacade;
 import com.github.starnowski.posjsonhelper.core.DatabaseOperationType;
-import com.github.starnowski.posjsonhelper.core.sql.ISQLDefinition;
-import com.github.starnowski.posjsonhelper.core.sql.SQLDefinitionFactoryFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 @Configuration
 public class SQLFunctionsConfiguration implements
@@ -23,12 +20,11 @@ public class SQLFunctionsConfiguration implements
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         Context context = Context.builder().build();
-        SQLDefinitionFactoryFacade sqlDefinitionFactoryFacade = new SQLDefinitionFactoryFacade();
-        List<ISQLDefinition> definitions = sqlDefinitionFactoryFacade.build(context);
+        DatabaseOperationExecutorFacade facade = new DatabaseOperationExecutorFacade();
         try {
-            new DatabaseOperationExecutor().execute(dataSource, definitions, DatabaseOperationType.LOG_ALL);
-            new DatabaseOperationExecutor().execute(dataSource, definitions, DatabaseOperationType.CREATE);
-            new DatabaseOperationExecutor().execute(dataSource, definitions, DatabaseOperationType.VALIDATE);
+            facade.execute(dataSource, context, DatabaseOperationType.LOG_ALL);
+            facade.execute(dataSource, context, DatabaseOperationType.CREATE);
+            facade.execute(dataSource, context, DatabaseOperationType.VALIDATE);
         } catch (Exception e) {
             throw new RuntimeException("Error during initialization of sql functions for jsonb type operations", e);
         }
