266ab14559e2a89163549d7000cdec4c848045fc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-26 23:33:39 +0200

    #13 - Tests code refactoring. [skip ci]

--- hibernate5/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate5/PostgreSQLDialectEnricherTest.groovy
+++ hibernate5/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate5/PostgreSQLDialectEnricherTest.groovy
@@ -1,10 +1,42 @@
 package com.github.starnowski.posjsonhelper.hibernate5
 
+
+import com.github.starnowski.posjsonhelper.core.Context
 import com.github.starnowski.posjsonhelper.core.CoreContextPropertiesSupplier
+import com.github.starnowski.posjsonhelper.core.HibernateContext
 import com.github.starnowski.posjsonhelper.core.HibernateContextPropertiesSupplier
+import com.github.starnowski.posjsonhelper.hibernate5.functions.JsonArrayFunction
+import org.hibernate.dialect.PostgreSQL81Dialect
+import org.hibernate.dialect.function.SQLFunction
+import org.hibernate.dialect.function.StandardSQLFunction
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.util.function.Function
+import java.util.stream.Collectors
 
 class PostgreSQLDialectEnricherTest extends Specification {
+
+    @Unroll
+    def "should enrich dialect with expected functions types" (){
+        given:
+            def coreContextPropertiesSupplier = Mock(CoreContextPropertiesSupplier)
+            def hibernateContextPropertiesSupplier = Mock(HibernateContextPropertiesSupplier)
+            def dialect = new PostgreSQL81Dialect()
+            coreContextPropertiesSupplier.get() >> context
+            hibernateContextPropertiesSupplier.get() >> hibernateContext
+            def tested = new PostgreSQLDialectEnricher(coreContextPropertiesSupplier, hibernateContextPropertiesSupplier)
+
+        when:
+            tested.enrich(dialect)
+
+        then:
+            dialect.getFunctions().entrySet().stream().filter({it -> expectedFunctionTypes.containsKey(it.getKey())}).collect(Collectors.toMap(new KeyMapper(), new ValueClassMapper())) == expectedFunctionTypes
+
+        where:
+            context |   hibernateContext    ||  expectedFunctionTypes
+            Context.builder().build()   |   HibernateContext.builder().build()  ||  ["jsonb_all_array_strings_exist" : StandardSQLFunction, "jsonb_any_array_strings_exist" : StandardSQLFunction, "json_function_json_array" : JsonArrayFunction]
+    }
 
     def "should have expected components initialized" (){
         given:
@@ -17,5 +49,21 @@ class PostgreSQLDialectEnricherTest extends Specification {
         then:
             coreContextPropertiesSupplier.getClass() == CoreContextPropertiesSupplier
             hibernateContextPropertiesSupplier.getClass() == HibernateContextPropertiesSupplier
+    }
+
+    private static class KeyMapper implements Function<Map.Entry<String, SQLFunction>, String>{
+
+        @Override
+        String apply(Map.Entry<String, SQLFunction> entry) {
+            return entry.getKey()
+        }
+    }
+
+    private static class ValueClassMapper implements Function<Map.Entry<String, SQLFunction>, Class>{
+
+        @Override
+        Class apply(Map.Entry<String, SQLFunction> entry) {
+            return entry.getValue().getClass()
+        }
     }
 }
