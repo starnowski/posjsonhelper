0576c133487fdc8432092f89c931cc3ec99c9922
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-10-31 00:19:35 +0100

    #16 - Test code refactoring [skip ci]

--- hibernate5/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate5/demo/dao/SQLInjectionItTest.groovy
+++ hibernate5/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate5/demo/dao/SQLInjectionItTest.groovy
@@ -6,18 +6,18 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlConfig
 import org.springframework.test.context.jdbc.SqlGroup
-import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
 import spock.lang.Unroll
 
 import static com.github.starnowski.posjsonhelper.hibernate5.demo.Application.CLEAR_DATABASE_SCRIPT_PATH
+import static com.github.starnowski.posjsonhelper.hibernate5.demo.Application.ITEMS_SCRIPT_PATH
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED
 
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SqlGroup([
-        @Sql(value = CLEAR_DATABASE_SCRIPT_PATH,
+        @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
                 config = @SqlConfig(transactionMode = ISOLATED),
                 executionPhase = BEFORE_TEST_METHOD),
         @Sql(value = CLEAR_DATABASE_SCRIPT_PATH,
@@ -26,23 +26,29 @@ import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.IS
 ])
 class SQLInjectionItTest extends Specification {
 
+    private static final String JSON_ALL_STATEMENT_PATTERN = "SELECT id FROM item WHERE jsonb_all_array_strings_exist(jsonb_extract_path(jsonb_content, %s), array[%s]) "
+
     @Autowired
     JdbcTemplate jdbcTemplate
 
     @Unroll
-    @Transactional(readOnly = true)
-    def "should set configuration property #property with expected value #value"(){
+    def "should not modify current configuration property #property with expected value #value when executing correct statement"(){
         given:
             jdbcTemplate.execute(String.format("SELECT set_config('%s', '%s', false);", property, value))
 
         when:
-            def result = jdbcTemplate.queryForObject(String.format("SELECT current_setting('%s')", property), String)
+            def result = jdbcTemplate.queryForObject(String.format(JSON_ALL_STATEMENT_PATTERN, "'top_element_with_set_of_values'", "'TAG1', 'TAG2'"), Integer)
 
         then:
-            result == value
+            def currentValue = jdbcTemplate.queryForObject(String.format("SELECT current_setting('%s')", property), String)
+            currentValue == value
+
+        and: "result should match"
+            result == 1
 
         where:
             property    |   value
             "c.prop1"   |   "some value"
+            "prop.value"   |   "this is a test"
     }
 }
