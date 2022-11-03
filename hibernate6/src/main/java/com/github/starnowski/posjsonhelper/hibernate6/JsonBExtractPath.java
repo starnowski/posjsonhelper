d73d53d80e936ba53de0e863c10fde4ef0b79366
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-11-03 10:11:39 +0100

    #24 - Added toolchains configuration [skip ci]

--- hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/JsonBExtractPath.java
+++ hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/JsonBExtractPath.java
@@ -5,10 +5,8 @@ import org.hibernate.query.sqm.tree.domain.SqmPath;
 
 import java.util.List;
 
-import static com.github.starnowski.posjsonhelper.core.Constants.JSONB_EXTRACT_PATH_FUNCTION_NAME;
-
 public class JsonBExtractPath extends AbstractJsonBExtractPath {
     public JsonBExtractPath(SqmPath<?> mapPath, NodeBuilder nodeBuilder, List<String> path) {
-        super(mapPath, nodeBuilder, path, JSONB_EXTRACT_PATH_FUNCTION_NAME);
+//        super(mapPath, nodeBuilder, path, JSONB_EXTRACT_PATH_FUNCTION_NAME);
     }
 }
--- hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/JsonBExtractPathText.java
+++ hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/JsonBExtractPathText.java
@@ -5,11 +5,9 @@ import org.hibernate.query.sqm.tree.domain.SqmPath;
 
 import java.util.List;
 
-import static com.github.starnowski.posjsonhelper.core.Constants.JSONB_EXTRACT_PATH_TEXT_FUNCTION_NAME;
-
 public class JsonBExtractPathText extends AbstractJsonBExtractPath {
 
     public JsonBExtractPathText(SqmPath<?> mapPath, NodeBuilder nodeBuilder, List<String> path) {
-        super(mapPath, nodeBuilder, path, JSONB_EXTRACT_PATH_TEXT_FUNCTION_NAME);
+//        super(mapPath, nodeBuilder, path, JSONB_EXTRACT_PATH_TEXT_FUNCTION_NAME);
     }
 }
