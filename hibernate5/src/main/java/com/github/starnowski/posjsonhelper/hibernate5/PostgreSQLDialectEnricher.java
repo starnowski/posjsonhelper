d07e787a7a857f713c57ca0f675694a88c782639
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-03 22:45:34 +0200

    #32 - Added javadoc to  PostgreSQLDialectEnricher [skip ci]

--- hibernate5/src/main/java/com/github/starnowski/posjsonhelper/hibernate5/PostgreSQLDialectEnricher.java
+++ hibernate5/src/main/java/com/github/starnowski/posjsonhelper/hibernate5/PostgreSQLDialectEnricher.java
@@ -11,7 +11,13 @@ import org.hibernate.type.StandardBasicTypes;
 
 public class PostgreSQLDialectEnricher {
 
+    /**
+     * Supplier for {@link Context} object based on system properties
+     */
     private final CoreContextPropertiesSupplier coreContextPropertiesSupplier;
+    /**
+     * Supplier for {@link HibernateContext} object based on system properties
+     */
     private final HibernateContextPropertiesSupplier hibernateContextPropertiesSupplier;
 
     public PostgreSQLDialectEnricher() {
@@ -27,6 +33,13 @@ public class PostgreSQLDialectEnricher {
         enrich(postgreSQL81Dialect, coreContextPropertiesSupplier.get(), hibernateContextPropertiesSupplier.get());
     }
 
+    /**
+     * Register function definitions based on {@link HibernateContext} and {@link Context} in {@link PostgreSQL81Dialect} object.
+     *
+     * @param postgreSQL81Dialect postgres dialect
+     * @param context core context based on which enricher component is going to register used functions.
+     * @param hibernateContext hibernate context based on which enricher component is going to register used functions.
+     */
     public void enrich(PostgreSQL81Dialect postgreSQL81Dialect, Context context, HibernateContext hibernateContext) {
         postgreSQL81Dialect.getFunctions().put(hibernateContext.getJsonFunctionJsonArrayOperator(), new JsonArrayFunction());
         postgreSQL81Dialect.getFunctions().put(hibernateContext.getJsonbAllArrayStringsExistOperator(), new StandardSQLFunction(context.getJsonbAllArrayStringsExistFunctionReference(), StandardBasicTypes.BOOLEAN));
