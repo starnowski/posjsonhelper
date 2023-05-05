f62782453a4e152b22b9eb95984102d977610164
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-06 01:34:28 +0200

    #32 - Added additional tests.

--- core/src/main/java/com/github/starnowski/posjsonhelper/core/HibernateContextPropertiesSupplier.java
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/HibernateContextPropertiesSupplier.java
@@ -23,6 +23,9 @@ package com.github.starnowski.posjsonhelper.core;
 
 import static com.github.starnowski.posjsonhelper.core.Constants.*;
 
+/**
+ * Supplier of  {@link HibernateContext} based on system properties.
+ */
 public class HibernateContextPropertiesSupplier {
 
     private final SystemPropertyReader systemPropertyReader;
@@ -35,6 +38,14 @@ public class HibernateContextPropertiesSupplier {
         this.systemPropertyReader = systemPropertyReader;
     }
 
+    /**
+     * Generates object to type {@link HibernateContext}, based on system properties.
+     * Component sets below properties:
+     * {@link HibernateContext#jsonbAllArrayStringsExistOperator} based on {@link Constants#JSONB_ALL_ARRAY_STRINGS_EXIST_HIBERNATE_OPERATOR_PROPERTY}
+     * {@link HibernateContext#jsonbAnyArrayStringsExistOperator} based on {@link Constants#JSONB_ANY_ARRAY_STRINGS_EXIST_HIBERNATE_OPERATOR_PROPERTY}
+     * {@link HibernateContext#jsonFunctionJsonArrayOperator} based on {@link Constants#JSON_FUNCTION_JSON_ARRAY_HIBERNATE_OPERATOR_PROPERTY}
+     * @return
+     */
     public HibernateContext get(){
         HibernateContext.ContextBuilder builder = HibernateContext.builder();
         String jsonbAllArrayStringsExist = systemPropertyReader.read(JSONB_ALL_ARRAY_STRINGS_EXIST_HIBERNATE_OPERATOR_PROPERTY);
