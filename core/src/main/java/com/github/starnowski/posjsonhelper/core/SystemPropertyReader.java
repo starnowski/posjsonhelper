aef0e014fc0f4f6bd0c82d0ef6ccfc8cc09f4559
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-05 23:12:42 +0200

    #32 - Added javadoc for the SystemPropertyReader. [skip ci]

--- core/src/main/java/com/github/starnowski/posjsonhelper/core/SystemPropertyReader.java
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/SystemPropertyReader.java
@@ -23,8 +23,16 @@ package com.github.starnowski.posjsonhelper.core;
 
 import static java.lang.System.getProperty;
 
+/**
+ * The facade that invokes static method {@link System#getProperty(String)}
+ */
 public class SystemPropertyReader {
 
+    /**
+     * Invokes {@link System#getProperty(String)} method for passed property
+     * @param property for which component should read value
+     * @return check method {@link System#getProperty(String)}
+     */
     public String read(String property) {
         return getProperty(property);
     }
