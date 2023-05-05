2f3903b78a9497e88990bdd7a34879c401a4ae20
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-05 20:43:08 +0200

    #32 - Added javadoc for the JsonbAnyArrayStringsExistPredicate. [skip ci]

--- hibernate5/src/main/java/com/github/starnowski/posjsonhelper/hibernate5/predicates/JsonbAnyArrayStringsExistPredicate.java
+++ hibernate5/src/main/java/com/github/starnowski/posjsonhelper/hibernate5/predicates/JsonbAnyArrayStringsExistPredicate.java
@@ -25,6 +25,10 @@ import com.github.starnowski.posjsonhelper.core.HibernateContext;
 import com.github.starnowski.posjsonhelper.hibernate5.JsonBExtractPath;
 import org.hibernate.query.criteria.internal.CriteriaBuilderImpl;
 
+/**
+ * Type that extends {@link AbstractJsonbArrayStringsExistPredicate}.
+ * Implemented of HQL function defined by method {@link HibernateContext#getJsonbAnyArrayStringsExistOperator()} }
+ */
 public class JsonbAnyArrayStringsExistPredicate extends AbstractJsonbArrayStringsExistPredicate {
     public JsonbAnyArrayStringsExistPredicate(HibernateContext context, CriteriaBuilderImpl criteriaBuilder, JsonBExtractPath jsonBExtractPath, String[] values) {
         super(context, criteriaBuilder, jsonBExtractPath, values);
