e9959f51ebc935a0e880a5d2510345ad84280020
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-06 02:45:44 +0200

    #32 - Added javadoc for the Constants type. [skip ci]

--- core/src/main/java/com/github/starnowski/posjsonhelper/core/Constants.java
+++ core/src/main/java/com/github/starnowski/posjsonhelper/core/Constants.java
@@ -29,11 +29,32 @@ public class Constants {
     public static final String DEFAULT_JSONB_ANY_ARRAY_STRINGS_EXIST_FUNCTION_NAME = "jsonb_any_array_strings_exist";
     public static final String JSONB_ALL_ARRAY_STRINGS_EXIST_FUNCTION_NAME_PROPERTY = "com.github.starnowski.posjsonhelper.core.functions.jsonb_all_array_strings_exist";
     public static final String JSONB_ANY_ARRAY_STRINGS_EXIST_FUNCTION_NAME_PROPERTY = "com.github.starnowski.posjsonhelper.core.functions.jsonb_any_array_strings_exist";
+    /**
+     * 	System property that is being used to set the name of database schema where the SQL functions should be created
+     */
     public static final String SCHEMA_PROPERTY = "com.github.starnowski.posjsonhelper.core.schema";
+    /**
+     * 	The default name for the HQL function that wraps SQL function defined in core context {@link Context#jsonbAllArrayStringsExistFunctionReference}.
+     */
     public static final String DEFAULT_JSONB_ALL_ARRAY_STRINGS_EXIST_HIBERNATE_OPERATOR = "jsonb_all_array_strings_exist";
+    /**
+     * 	The default name for the HQL function that wraps SQL function defined in core context {@link Context#jsonbAnyArrayStringsExistFunctionReference}.
+     */
     public static final String DEFAULT_JSONB_ANY_ARRAY_STRINGS_EXIST_HIBERNATE_OPERATOR = "jsonb_any_array_strings_exist";
+    /**
+     * 	The default name for the HQL function that wraps Postgres "ARRAY" operator.
+     */
     public static final String DEFAULT_JSON_FUNCTION_JSON_ARRAY_HIBERNATE_OPERATOR = "json_function_json_array";
+    /**
+     * System property that is being used to set the name of HQL function that invokes SQL function defined in core context {@link Context#jsonbAllArrayStringsExistFunctionReference}.
+     */
     public static final String JSONB_ALL_ARRAY_STRINGS_EXIST_HIBERNATE_OPERATOR_PROPERTY = "com.github.starnowski.posjsonhelper.core.hibernate.functions.jsonb_all_array_strings_exist";
+    /**
+     * System property that is being used to set the name of HQL function that invokes SQL function defined in core context {@link Context#jsonbAnyArrayStringsExistFunctionReference}.
+     */
     public static final String JSONB_ANY_ARRAY_STRINGS_EXIST_HIBERNATE_OPERATOR_PROPERTY = "com.github.starnowski.posjsonhelper.core.hibernate.functions.jsonb_any_array_strings_exist";
+    /**
+     * System property that is being used to set the name of HQL function that wraps the array operator in Postgresql.
+     */
     public static final String JSON_FUNCTION_JSON_ARRAY_HIBERNATE_OPERATOR_PROPERTY = "com.github.starnowski.posjsonhelper.core.hibernate.functions.json_function_json_array";
 }
