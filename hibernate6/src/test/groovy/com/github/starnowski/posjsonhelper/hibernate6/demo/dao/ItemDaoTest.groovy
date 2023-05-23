976f313039070c5fc7a53abd15ce02597b4575e5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-23 18:46:39 +0200

    #24 - Added tests [skip ci]

--- hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/ItemDaoTest.groovy
+++ hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/ItemDaoTest.groovy
@@ -19,7 +19,6 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TE
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED
 
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@WebMvcTest
 @SqlGroup([
         @Sql(value = CLEAR_DATABASE_SCRIPT_PATH,
                 config = @SqlConfig(transactionMode = ISOLATED),
@@ -35,77 +34,77 @@ class ItemDaoTest extends Specification {
     @Autowired
     private ItemDao tested
 
-//    @Unroll
-//    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
-//            config = @SqlConfig(transactionMode = ISOLATED),
-//            executionPhase = BEFORE_TEST_METHOD)
-//    def "should return single correct id #expectedId when searching by all matching tags [#tags]" () {
-//        when:
-//        def results = tested.findAllByAllMatchingTags(new HashSet<String>(tags))
-//
-//        then:
-//        results.stream().map({it.getId()}).collect(Collectors.toSet()) == new HashSet([expectedId ])
-//
-//        where:
-//        tags                    ||  expectedId
-//        ['TAG1', 'TAG2']        ||  1
-//        ['TAG11']               ||  1
-//        ['TAG12']               ||  1
-//    }
-//
-//    @Unroll
-//    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
-//            config = @SqlConfig(transactionMode = ISOLATED),
-//            executionPhase = BEFORE_TEST_METHOD)
-//    def "should return correct id #expectedIds when searching by all matching tags [#tags]" () {
-//        when:
-//        def results = tested.findAllByAllMatchingTags(new HashSet<String>(tags))
-//
-//        then:
-//        results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
-//
-//        where:
-//        tags                            ||  expectedIds
-//        ['TAG1', 'TAG2']                ||  [1].toSet()
-//        ['TAG3']                        ||  [3, 2].toSet()
-//        ['TAG21', 'TAG22']              ||  [1, 4].toSet()
-//    }
-//
-//    @Unroll
-//    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
-//            config = @SqlConfig(transactionMode = ISOLATED),
-//            executionPhase = BEFORE_TEST_METHOD)
-//    def "should return correct id except #expectedIds when searching item that do not match by all matching tags [#tags]" () {
-//        when:
-//        def results = tested.findAllThatDoNotMatchByAllMatchingTags(new HashSet<String>(tags))
-//
-//        then:
-//        results.stream().map({it.getId()}).collect(Collectors.toSet()) == ALL_ITEMS_IDS.stream().filter({id -> !expectedIds.contains(id)}).collect(Collectors.toSet())
-//
-//        where:
-//        tags                            ||  expectedIds
-//        ['TAG1', 'TAG2']                ||  [1L].toSet()
-//        ['TAG3']                        ||  [3L, 2L].toSet()
-//        ['TAG21', 'TAG22']              ||  [1L, 4L].toSet()
-//    }
+    @Unroll
+    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    def "should return single correct id #expectedId when searching by all matching tags [#tags]" () {
+        when:
+            def results = tested.findAllByAllMatchingTags(new HashSet<String>(tags))
 
-//    @Unroll
-//    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
-//            config = @SqlConfig(transactionMode = ISOLATED),
-//            executionPhase = BEFORE_TEST_METHOD)
-//    def "should return correct id #expectedIds when searching by any matching tags [#tags]" () {
-//        when:
-//        def results = tested.findAllByAnyMatchingTags(new HashSet<String>(tags))
-//
-//        then:
-//        results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
-//
-//        where:
-//        tags                            ||  expectedIds
-//        ['TAG1', 'TAG2']                ||  [1, 3].toSet()
-//        ['TAG3']                        ||  [3, 2].toSet()
-//        ['TAG1', 'TAG32']               ||  [1, 3, 5].toSet()
-//    }
+        then:
+            results.stream().map({it.getId()}).collect(Collectors.toSet()) == new HashSet([expectedId ])
+
+        where:
+            tags                    ||  expectedId
+            ['TAG1', 'TAG2']        ||  1
+            ['TAG11']               ||  1
+            ['TAG12']               ||  1
+    }
+
+    @Unroll
+    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    def "should return correct id #expectedIds when searching by all matching tags [#tags]" () {
+        when:
+            def results = tested.findAllByAllMatchingTags(new HashSet<String>(tags))
+
+        then:
+            results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
+
+        where:
+            tags                            ||  expectedIds
+            ['TAG1', 'TAG2']                ||  [1].toSet()
+            ['TAG3']                        ||  [3, 2].toSet()
+            ['TAG21', 'TAG22']              ||  [1, 4].toSet()
+    }
+
+    @Unroll
+    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    def "should return correct id except #expectedIds when searching item that do not match by all matching tags [#tags]" () {
+        when:
+            def results = tested.findAllThatDoNotMatchByAllMatchingTags(new HashSet<String>(tags))
+
+        then:
+            results.stream().map({it.getId()}).collect(Collectors.toSet()) == ALL_ITEMS_IDS.stream().filter({id -> !expectedIds.contains(id)}).collect(Collectors.toSet())
+
+        where:
+            tags                            ||  expectedIds
+            ['TAG1', 'TAG2']                ||  [1L].toSet()
+            ['TAG3']                        ||  [3L, 2L].toSet()
+            ['TAG21', 'TAG22']              ||  [1L, 4L].toSet()
+    }
+
+    @Unroll
+    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    def "should return correct id #expectedIds when searching by any matching tags [#tags]" () {
+        when:
+            def results = tested.findAllByAnyMatchingTags(new HashSet<String>(tags))
+
+        then:
+            results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
+
+        where:
+            tags                            ||  expectedIds
+            ['TAG1', 'TAG2']                ||  [1, 3].toSet()
+            ['TAG3']                        ||  [3, 2].toSet()
+            ['TAG1', 'TAG32']               ||  [1, 3, 5].toSet()
+    }
 
     @Unroll
     @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
@@ -113,61 +112,61 @@ class ItemDaoTest extends Specification {
             executionPhase = BEFORE_TEST_METHOD)
     def "should return correct id #expectedIds when searching by #operator operator to compare double value #value" () {
         when:
-        def results = tested.findAllByNumericValue(value, operator)
-        //https://www.cinqict.nl/blog/how-to-run-spock-with-the-newest-java-versions-without
-        //https://stackoverflow.com/questions/63265708/how-to-configure-spock-in-spring-boot-with-gradle-6-and-java-11
-        //https://spockframework.org/spock/docs/1.2/release_notes.html
+            def results = tested.findAllByNumericValue(value, operator)
+            //https://www.cinqict.nl/blog/how-to-run-spock-with-the-newest-java-versions-without
+            //https://stackoverflow.com/questions/63265708/how-to-configure-spock-in-spring-boot-with-gradle-6-and-java-11
+            //https://spockframework.org/spock/docs/1.2/release_notes.html
 
         then:
-        results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
+            results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
 
         where:
-        operator    |   value       ||  expectedIds
-        EQ          |   -1137.98    ||  [11].toSet()
-        EQ          |   353.01      ||  [10].toSet()
-        GE          |   -1137.98    ||  [10, 11, 12].toSet()
-        GT          |   -1137.98    ||  [10, 12].toSet()
-        LE          |   -1137.98    ||  [11].toSet()
-        LT          |   -1137.98    ||  [].toSet()
-        LT          |   20490.04    ||  [10, 11].toSet()
+            operator    |   value       ||  expectedIds
+            EQ          |   -1137.98    ||  [11].toSet()
+            EQ          |   353.01      ||  [10].toSet()
+            GE          |   -1137.98    ||  [10, 11, 12].toSet()
+            GT          |   -1137.98    ||  [10, 12].toSet()
+            LE          |   -1137.98    ||  [11].toSet()
+            LT          |   -1137.98    ||  [].toSet()
+            LT          |   20490.04    ||  [10, 11].toSet()
     }
 
-//    @Unroll
-//    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
-//            config = @SqlConfig(transactionMode = ISOLATED),
-//            executionPhase = BEFORE_TEST_METHOD)
-//    def "should return correct id #expectedIds when searching by IN operator to compare enum value #values" () {
-//        when:
-//        def results = tested.findAllByStringThatMatchInValues(values)
-//
-//        then:
-//        results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
-//
-//        where:
-//        values                              ||  expectedIds
-//        ['SUPER', 'USER']                   ||  [14, 13].toSet()
-//        ['SUPER']                           ||  [13].toSet()
-//        ['ANONYMOUS', 'SUPER']              ||  [15, 13].toSet()
-//    }
-//
-//    @Unroll
-//    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
-//            config = @SqlConfig(transactionMode = ISOLATED),
-//            executionPhase = BEFORE_TEST_METHOD)
-//    def "should return correct id #expectedIds when searching by LIKE operator with #expresion" () {
-//        when:
-//        def results = tested.findAllByStringValueAndLikeOperator(expresion)
-//
-//        then:
-//        results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
-//
-//        where:
-//        expresion                           ||  expectedIds
-//        'this is full sentence'             ||  [16].toSet()
-//        'this is '                          ||  [].toSet()
-//        'this is %'                         ||  [16, 17].toSet()
-//        'end of'                            ||  [].toSet()
-//        'end of%'                           ||  [].toSet()
-//        '%end of%'                          ||  [18].toSet()
-//    }
+    @Unroll
+    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    def "should return correct id #expectedIds when searching by IN operator to compare enum value #values" () {
+        when:
+            def results = tested.findAllByStringThatMatchInValues(values)
+
+        then:
+            results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
+
+        where:
+            values                              ||  expectedIds
+            ['SUPER', 'USER']                   ||  [14, 13].toSet()
+            ['SUPER']                           ||  [13].toSet()
+            ['ANONYMOUS', 'SUPER']              ||  [15, 13].toSet()
+    }
+
+    @Unroll
+    @Sql(value = [CLEAR_DATABASE_SCRIPT_PATH, ITEMS_SCRIPT_PATH],
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    def "should return correct id #expectedIds when searching by LIKE operator with #expresion" () {
+        when:
+            def results = tested.findAllByStringValueAndLikeOperator(expresion)
+
+        then:
+            results.stream().map({it.getId()}).collect(Collectors.toSet()) == expectedIds
+
+        where:
+            expresion                           ||  expectedIds
+            'this is full sentence'             ||  [16].toSet()
+            'this is '                          ||  [].toSet()
+            'this is %'                         ||  [16, 17].toSet()
+            'end of'                            ||  [].toSet()
+            'end of%'                           ||  [].toSet()
+            '%end of%'                          ||  [18].toSet()
+    }
 }
--- hibernate6/src/test/java/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/ItemDao.java
+++ hibernate6/src/test/java/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/ItemDao.java
@@ -27,34 +27,34 @@ public class ItemDao {
     @Autowired
     private EntityManager entityManager;
 
-//    public List<Item> findAllByAllMatchingTags(Set<String> tags) {
-//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-//        CriteriaQuery<Item> query = cb.createQuery(Item.class);
-//        Root<Item> root = query.from(Item.class);
-//        query.select(root);
-//        query.where(new JsonbAllArrayStringsExistPredicate(hibernateContext, (CriteriaBuilderImpl) cb, new JsonBExtractPath((CriteriaBuilderImpl) cb, singletonList("top_element_with_set_of_values"), root.get("jsonbContent")), tags.toArray(new String[0])));
-//        return entityManager.createQuery(query).getResultList();
-//    }
+    public List<Item> findAllByAllMatchingTags(Set<String> tags) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> query = cb.createQuery(Item.class);
+        Root<Item> root = query.from(Item.class);
+        query.select(root);
+        query.where(new JsonbAllArrayStringsExistPredicate(hibernateContext, (CriteriaBuilderImpl) cb, new JsonBExtractPath((CriteriaBuilderImpl) cb, singletonList("top_element_with_set_of_values"), root.get("jsonbContent")), tags.toArray(new String[0])));
+        return entityManager.createQuery(query).getResultList();
+    }
 
-//    public List<Item> findAllThatDoNotMatchByAllMatchingTags(Set<String> tags) {
-//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-//        CriteriaQuery<Item> query = cb.createQuery(Item.class);
-//        Root<Item> root = query.from(Item.class);
-//        query.select(root);
-//        Predicate notAllMatchingTags = cb.not(new JsonbAllArrayStringsExistPredicate(hibernateContext, (CriteriaBuilderImpl) cb, new JsonBExtractPath((CriteriaBuilderImpl) cb, singletonList("top_element_with_set_of_values"), root.get("jsonbContent")), tags.toArray(new String[0])));
-//        Predicate withoutSetOfValuesProperty = cb.isNull(new JsonBExtractPath((CriteriaBuilderImpl) cb, singletonList("top_element_with_set_of_values"), root.get("jsonbContent")));
-//        query.where(cb.or(withoutSetOfValuesProperty, notAllMatchingTags));
-//        return entityManager.createQuery(query).getResultList();
-//    }
+    public List<Item> findAllThatDoNotMatchByAllMatchingTags(Set<String> tags) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> query = cb.createQuery(Item.class);
+        Root<Item> root = query.from(Item.class);
+        query.select(root);
+        Predicate notAllMatchingTags = cb.not(new JsonbAllArrayStringsExistPredicate(hibernateContext, (CriteriaBuilderImpl) cb, new JsonBExtractPath((CriteriaBuilderImpl) cb, singletonList("top_element_with_set_of_values"), root.get("jsonbContent")), tags.toArray(new String[0])));
+        Predicate withoutSetOfValuesProperty = cb.isNull(new JsonBExtractPath((CriteriaBuilderImpl) cb, singletonList("top_element_with_set_of_values"), root.get("jsonbContent")));
+        query.where(cb.or(withoutSetOfValuesProperty, notAllMatchingTags));
+        return entityManager.createQuery(query).getResultList();
+    }
 
-//    public List<Item> findAllByAnyMatchingTags(HashSet<String> tags) {
-//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-//        CriteriaQuery<Item> query = cb.createQuery(Item.class);
-//        Root<Item> root = query.from(Item.class);
-//        query.select(root);
-//        query.where(new JsonbAnyArrayStringsExistPredicate(hibernateContext, (CriteriaBuilderImpl) cb, new JsonBExtractPath((CriteriaBuilderImpl) cb, singletonList("top_element_with_set_of_values"), root.get("jsonbContent")), tags.toArray(new String[0])));
-//        return entityManager.createQuery(query).getResultList();
-//    }
+    public List<Item> findAllByAnyMatchingTags(HashSet<String> tags) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> query = cb.createQuery(Item.class);
+        Root<Item> root = query.from(Item.class);
+        query.select(root);
+        query.where(new JsonbAnyArrayStringsExistPredicate(hibernateContext, (CriteriaBuilderImpl) cb, new JsonBExtractPath((CriteriaBuilderImpl) cb, singletonList("top_element_with_set_of_values"), root.get("jsonbContent")), tags.toArray(new String[0])));
+        return entityManager.createQuery(query).getResultList();
+    }
 
     public List<Item> findAllByNumericValue(BigDecimal bigDecimal, NumericComparator numericComparator) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
@@ -82,22 +82,22 @@ public class ItemDao {
         }
         return entityManager.createQuery(query).getResultList();
     }
-//
-//    public List<Item> findAllByStringThatMatchInValues(List<String> strings) {
-//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-//        CriteriaQuery<Item> query = cb.createQuery(Item.class);
-//        Root<Item> root = query.from(Item.class);
-//        query.select(root);
-//        query.where((new JsonBExtractPathText((CriteriaBuilderImpl) cb, singletonList("enum_value"), root.get("jsonbContent"))).in(strings));
-//        return entityManager.createQuery(query).getResultList();
-//    }
 
-//    public List<Item> findAllByStringValueAndLikeOperator(String expression) {
-//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-//        CriteriaQuery<Item> query = cb.createQuery(Item.class);
-//        Root<Item> root = query.from(Item.class);
-//        query.select(root);
-//        query.where(cb.like(new JsonBExtractPathText((CriteriaBuilderImpl) cb, singletonList("string_value"), root.get("jsonbContent")), expression));
-//        return entityManager.createQuery(query).getResultList();
-//    }
+    public List<Item> findAllByStringThatMatchInValues(List<String> strings) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> query = cb.createQuery(Item.class);
+        Root<Item> root = query.from(Item.class);
+        query.select(root);
+        query.where((new JsonBExtractPathText((CriteriaBuilderImpl) cb, singletonList("enum_value"), root.get("jsonbContent"))).in(strings));
+        return entityManager.createQuery(query).getResultList();
+    }
+
+    public List<Item> findAllByStringValueAndLikeOperator(String expression) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> query = cb.createQuery(Item.class);
+        Root<Item> root = query.from(Item.class);
+        query.select(root);
+        query.where(cb.like(new JsonBExtractPathText((CriteriaBuilderImpl) cb, singletonList("string_value"), root.get("jsonbContent")), expression));
+        return entityManager.createQuery(query).getResultList();
+    }
 }
