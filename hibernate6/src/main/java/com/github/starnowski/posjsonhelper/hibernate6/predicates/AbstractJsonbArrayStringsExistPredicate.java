3ee32393a095101dd8151b70d5fa8e1a8104b708
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-23 20:17:15 +0200

    #24 - Code refactoring [skip ci]

--- hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/predicates/AbstractJsonbArrayStringsExistPredicate.java
+++ hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/predicates/AbstractJsonbArrayStringsExistPredicate.java
@@ -1,36 +1,39 @@
 /**
- *     Posjsonhelper library is an open-source project that adds support of
- *     Hibernate query for https://www.postgresql.org/docs/10/functions-json.html)
- *
- *     Copyright (C) 2023  Szymon Tarnowski
- *
- *     This library is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU Lesser General Public
- *     License as published by the Free Software Foundation; either
- *     version 2.1 of the License, or (at your option) any later version.
- *
- *     This library is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *     Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with this library; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- *     USA
+ * Posjsonhelper library is an open-source project that adds support of
+ * Hibernate query for https://www.postgresql.org/docs/10/functions-json.html)
+ * <p>
+ * Copyright (C) 2023  Szymon Tarnowski
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 package com.github.starnowski.posjsonhelper.hibernate6.predicates;
 
 import com.github.starnowski.posjsonhelper.core.HibernateContext;
+import com.github.starnowski.posjsonhelper.hibernate6.AbstractJsonBExtractPath;
 import com.github.starnowski.posjsonhelper.hibernate6.JsonBExtractPath;
 import org.hibernate.query.sqm.NodeBuilder;
 import org.hibernate.query.sqm.tree.expression.SqmExpression;
-import org.hibernate.query.sqm.tree.predicate.AbstractNegatableSqmPredicate;
+import org.hibernate.query.sqm.tree.predicate.SqmInListPredicate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * The component renders arguments in the below form. Based on string arguments, JsonPath, and main function.
@@ -39,23 +42,22 @@ import java.util.List;
  * <pre>{@code
  * {{main_func}}( jsonb_extract_path( generatedAlias0.jsonbContent , :param0 ) , json_function_json_array(:param1, :param2)) = TRUE
  * }</pre>
- *
+ * <p>
  * where:
  * {{main_func}} - name of main function returned by method {@link #getFunctionName()}
  * jsonb_extract_path( generatedAlias0.jsonbContent , :param0 ) - json path part, with this example path has only one element normally this could part could contain more elements "param"
  * {{json_function_json_array}} - hibernate operator that wraps the "array" operator in postgres. Values comes from  {@link HibernateContext#getJsonFunctionJsonArrayOperator()}
  * (:param1, :param2) - rendered string arguments
  * TRUE - expected predicate value, by default it is "TRUE"
- *
  */
-public abstract class AbstractJsonbArrayStringsExistPredicate extends AbstractNegatableSqmPredicate {
+public abstract class AbstractJsonbArrayStringsExistPredicate<T extends AbstractJsonbArrayStringsExistPredicate> extends SqmInListPredicate<String> {
 
     private final HibernateContext context;
     private final JsonBExtractPath jsonBExtractPath;
     private final List<SqmExpression> values;
 
     public AbstractJsonbArrayStringsExistPredicate(HibernateContext context, NodeBuilder nodeBuilder, JsonBExtractPath jsonBExtractPath, String[] values, boolean negated) {
-        super(negated, nodeBuilder);
+        super(jsonBExtractPath, parameters(nodeBuilder, values), negated, nodeBuilder);
         this.jsonBExtractPath = jsonBExtractPath;
         this.context = context;
         Iterator var6 = Arrays.asList(values).iterator();
@@ -64,6 +66,15 @@ public abstract class AbstractJsonbArrayStringsExistPredicate extends AbstractNe
             Object value = var6.next();
             this.values.add(nodeBuilder.literal(value));
         }
+    }
+
+    private static List<? extends SqmExpression<String>> parameters(NodeBuilder nodeBuilder, String... values) {
+        if (values == null || values.length == 0) {
+            throw new IllegalArgumentException("Values can not be null or empty list");
+        }
+        List<SqmExpression<String>> result = new ArrayList<>();
+        result.addAll(Stream.of(values).map(p -> nodeBuilder.literal(p)).collect(Collectors.toList()));
+        return result;
     }
 
 //    private String renderValues(RenderingContext renderingContext) {
@@ -83,9 +94,12 @@ public abstract class AbstractJsonbArrayStringsExistPredicate extends AbstractNe
 
     /**
      * Main HQL function for predicate.
+     *
      * @return name of the HQL function
      */
     abstract protected String getFunctionName();
+
+    abstract protected T generateCopy(HibernateContext context, NodeBuilder nodeBuilder, JsonBExtractPath jsonBExtractPath, String[] values, boolean negated);
 
     protected HibernateContext getContext() {
         return context;
--- hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/predicates/JsonbAnyArrayStringsExistPredicate.java
+++ hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/predicates/JsonbAnyArrayStringsExistPredicate.java
@@ -1,42 +1,46 @@
 /**
- *     Posjsonhelper library is an open-source project that adds support of
- *     Hibernate query for https://www.postgresql.org/docs/10/functions-json.html)
- *
- *     Copyright (C) 2023  Szymon Tarnowski
- *
- *     This library is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU Lesser General Public
- *     License as published by the Free Software Foundation; either
- *     version 2.1 of the License, or (at your option) any later version.
- *
- *     This library is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *     Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with this library; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- *     USA
+ * Posjsonhelper library is an open-source project that adds support of
+ * Hibernate query for https://www.postgresql.org/docs/10/functions-json.html)
+ * <p>
+ * Copyright (C) 2023  Szymon Tarnowski
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 package com.github.starnowski.posjsonhelper.hibernate6.predicates;
 
 import com.github.starnowski.posjsonhelper.core.HibernateContext;
-import com.github.starnowski.posjsonhelper.hibernate5.JsonBExtractPath;
 import com.github.starnowski.posjsonhelper.hibernate6.JsonBExtractPath;
-import org.hibernate.query.criteria.internal.CriteriaBuilderImpl;
 import org.hibernate.query.sqm.NodeBuilder;
 
 /**
  * Type that extends {@link AbstractJsonbArrayStringsExistPredicate}.
  * Implemented of HQL function defined by method {@link HibernateContext#getJsonbAnyArrayStringsExistOperator()} }
  */
-public class JsonbAnyArrayStringsExistPredicate extends AbstractJsonbArrayStringsExistPredicate {
+public class JsonbAnyArrayStringsExistPredicate extends AbstractJsonbArrayStringsExistPredicate<JsonbAnyArrayStringsExistPredicate> {
     public JsonbAnyArrayStringsExistPredicate(HibernateContext context, NodeBuilder nodeBuilder, JsonBExtractPath jsonBExtractPath, String[] values, boolean negated) {
         super(context, nodeBuilder, jsonBExtractPath, values, negated);
     }
+
     @Override
     protected String getFunctionName() {
         return getContext().getJsonbAnyArrayStringsExistOperator();
+    }
+
+    @Override
+    protected JsonbAnyArrayStringsExistPredicate generateCopy(HibernateContext context, NodeBuilder nodeBuilder, JsonBExtractPath jsonBExtractPath, String[] values, boolean negated) {
+        return new JsonbAnyArrayStringsExistPredicate(context, nodeBuilder, jsonBExtractPath, values, negated);
     }
 }
