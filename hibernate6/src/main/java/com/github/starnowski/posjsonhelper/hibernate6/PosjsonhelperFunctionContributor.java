e18e2b330ff15fbad123bf79cdb5e18c6c068990
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-25 09:50:34 +0200

    #24 - Added FunctionContributor (not yet implemented) [skip ci]

---
+++ hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/PosjsonhelperFunctionContributor.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.posjsonhelper.hibernate6;
+
+import org.hibernate.boot.model.FunctionContributions;
+import org.hibernate.boot.model.FunctionContributor;
+
+public class PosjsonhelperFunctionContributor implements FunctionContributor {
+    @Override
+    public void contributeFunctions(FunctionContributions functionContributions) {
+        //TODO
+    }
+}
