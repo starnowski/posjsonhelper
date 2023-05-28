62d2be927b924fd92c6134a6661f6612e70d969d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-28 17:18:07 +0200

    #24 - Added test cases [skip ci]

--- hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/SqmFunctionRegistryEnricherTest.groovy
+++ hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/SqmFunctionRegistryEnricherTest.groovy
@@ -16,24 +16,36 @@ import java.util.stream.Collectors
 class SqmFunctionRegistryEnricherTest extends Specification {
 
     @Unroll
-    def "should enrich sqmFunctionRegistry with expected functions types #expectedFunctionTypes" (){
+    def "should enrich sqmFunctionRegistry with expected functions types #expectedFunctionTypes"() {
         given:
-            def coreContextPropertiesSupplier = Mock(CoreContextPropertiesSupplier)
-            def hibernateContextPropertiesSupplier = Mock(HibernateContextPropertiesSupplier)
-            def sqmFunctionRegistry = new SqmFunctionRegistry()
-            coreContextPropertiesSupplier.get() >> Context.builder().build()
-            hibernateContextPropertiesSupplier.get() >> hibernateContext
-            def tested = new SqmFunctionRegistryEnricher(coreContextPropertiesSupplier, hibernateContextPropertiesSupplier)
+        def coreContextPropertiesSupplier = Mock(CoreContextPropertiesSupplier)
+        def hibernateContextPropertiesSupplier = Mock(HibernateContextPropertiesSupplier)
+        def sqmFunctionRegistry = new SqmFunctionRegistry()
+        coreContextPropertiesSupplier.get() >> context
+        hibernateContextPropertiesSupplier.get() >> hibernateContext
+        def tested = new SqmFunctionRegistryEnricher(coreContextPropertiesSupplier, hibernateContextPropertiesSupplier)
 
         when:
-            tested.enrich(sqmFunctionRegistry)
+        tested.enrich(sqmFunctionRegistry)
 
         then:
-            sqmFunctionRegistry.getFunctions().entrySet().stream().filter({it -> expectedFunctionTypes.containsKey(it.getKey())}).collect(Collectors.toMap(new KeyMapper(), new ValueClassMapper())) == expectedFunctionTypes
+        sqmFunctionRegistry.getFunctions().entrySet().stream().filter({ it -> expectedFunctionTypes.containsKey(it.getKey()) }).collect(Collectors.toMap(new KeyMapper(), new ValueClassMapper())) == expectedFunctionTypes
 
         where:
-            hibernateContext    ||  expectedFunctionTypes
-            HibernateContext.builder().build() ||  ["jsonb_all_array_strings_exist": "jsonb_all_array_strings_exist", "jsonb_any_array_strings_exist": "jsonb_any_array_strings_exist", "json_function_json_array": "array", "jsonb_extract_path": "jsonb_extract_path", "jsonb_extract_path_text": "jsonb_extract_path_text"]
+            hibernateContext    | context                   || expectedFunctionTypes
+            hcBuilder().build() | cBuilder().build()        || ["jsonb_all_array_strings_exist": "jsonb_all_array_strings_exist", "jsonb_any_array_strings_exist": "jsonb_any_array_strings_exist", "json_function_json_array": "array", "jsonb_extract_path": "jsonb_extract_path", "jsonb_extract_path_text": "jsonb_extract_path_text"]
+            hcBuilder().withJsonbAllArrayStringsExistOperator("jsonb_all").build() | cBuilder().build()        || ["jsonb_all": "jsonb_all_array_strings_exist", "jsonb_any_array_strings_exist": "jsonb_any_array_strings_exist", "json_function_json_array": "array", "jsonb_extract_path": "jsonb_extract_path", "jsonb_extract_path_text": "jsonb_extract_path_text"]
+            hcBuilder().withJsonbAllArrayStringsExistOperator("jsonb_all").build() | cBuilder().withJsonbAllArrayStringsExistFunctionReference("json_all_call").build()        || ["jsonb_all": "json_all_call", "jsonb_any_array_strings_exist": "jsonb_any_array_strings_exist", "json_function_json_array": "array", "jsonb_extract_path": "jsonb_extract_path", "jsonb_extract_path_text": "jsonb_extract_path_text"]
+    }
+
+    private static HibernateContext.ContextBuilder hcBuilder()
+    {
+        HibernateContext.builder()
+    }
+
+    private static Context.ContextBuilder cBuilder()
+    {
+        Context.builder()
     }
 
     private static class KeyMapper implements Function<Map.Entry<String, SqmFunctionDescriptor>, String> {
