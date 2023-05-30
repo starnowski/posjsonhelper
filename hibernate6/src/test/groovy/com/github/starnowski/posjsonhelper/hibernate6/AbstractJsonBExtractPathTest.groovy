af7218e39eaef48966f31d71fd14ca1f6442b3d6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-30 18:39:53 +0200

    #24 - Added the tests (not yet finished!) [skip ci]

--- hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/AbstractJsonBExtractPathTest.groovy
+++ hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/AbstractJsonBExtractPathTest.groovy
@@ -6,7 +6,13 @@ import org.hibernate.query.sqm.NodeBuilder
 import org.hibernate.query.sqm.function.FunctionRenderingSupport
 import org.hibernate.query.sqm.function.SqmFunctionDescriptor
 import org.hibernate.query.sqm.function.SqmFunctionRegistry
+import org.hibernate.query.sqm.internal.SqmCriteriaNodeBuilder
 import org.hibernate.query.sqm.tree.SqmTypedNode
+import org.hibernate.query.sqm.tree.domain.SqmBasicValuedSimplePath
+import org.hibernate.type.BasicTypeReference
+import org.hibernate.type.BasicTypeRegistry
+import org.hibernate.type.StandardBasicTypes
+import org.hibernate.type.spi.TypeConfiguration
 import org.mockito.Mockito
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -16,25 +22,36 @@ abstract class AbstractJsonBExtractPathTest <T extends AbstractJsonBExtractPath>
     @Unroll
     def "should pass correct path #path"(){
         given:
-            NodeBuilder nodeBuilder = Mock(NodeBuilder)
-            Path referencedPathSource = Mock(Path)
+            SqmCriteriaNodeBuilder nodeBuilder = Mock(SqmCriteriaNodeBuilder)
+            SqmBasicValuedSimplePath referencedPathSource = Mock(SqmBasicValuedSimplePath)
 
             QueryEngine queryEngine = Mockito.mock(QueryEngine)
-//            QueryEngine queryEngine = GroovyMock(QueryEngine, global: true, useObjenesis: false)
             SqmFunctionRegistry sqmFunctionRegistry = Mock(SqmFunctionRegistry)
             TestInterfaceThatImplementsSqmFunctionDescriptorAndFunctionRenderingSupport ti = Mock(TestInterfaceThatImplementsSqmFunctionDescriptorAndFunctionRenderingSupport)
             Mockito.when(queryEngine.getSqmFunctionRegistry()).thenReturn(sqmFunctionRegistry)
+            TypeConfiguration typeConfiguration = Mock(TypeConfiguration)
+            BasicTypeRegistry basicTypeRegistry = Mock(BasicTypeRegistry)
+            org.hibernate.type.BasicType basicType = Mock(org.hibernate.type.BasicType)
+            List<? extends SqmTypedNode<?>> expectedArguments = new ArrayList<>()
+            expectedArguments.add(referencedPathSource)
+            for (String p : path) {
+                org.hibernate.query.sqm.tree.expression.SqmExpression argument = Mock(org.hibernate.query.sqm.tree.expression.SqmExpression)
+                expectedArguments.add(argument)
+                nodeBuilder.value(p) >> argument
+            }
 
         when:
             T tested = prepareTestObject(referencedPathSource, nodeBuilder, path)
 
         then:
             2 * nodeBuilder.getQueryEngine() >> queryEngine
-//            2 * queryEngine.getSqmFunctionRegistry() >> sqmFunctionRegistry
             2 * sqmFunctionRegistry.findFunctionDescriptor(expectedFunctionName()) >> ti
+            1 * nodeBuilder.getTypeConfiguration() >> typeConfiguration
+            1 * typeConfiguration.getBasicTypeRegistry() >> basicTypeRegistry
+            1 * basicTypeRegistry.resolve(StandardBasicTypes.STRING) >> basicType
             tested.nodeBuilder().is(nodeBuilder)
             tested.getFunctionName() == expectedFunctionName()
-            List<SqmTypedNode> arguments = tested.getArguments()
+            tested.getArguments() == expectedArguments
 
         where:
             path << [["some", "property"], ["child1", "grandson1"]]
