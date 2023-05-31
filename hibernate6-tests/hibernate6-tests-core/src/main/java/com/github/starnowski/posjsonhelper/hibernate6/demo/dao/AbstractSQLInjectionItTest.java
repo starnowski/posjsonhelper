71039dfcbf7100ede3afecbb3b3c43017d7a6bac
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-05-31 20:28:11 +0200

    #24 - Added tests cases for sql injection (not yet finished) [skip ci]

--- hibernate6-tests/hibernate6-tests-core/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/AbstractSQLInjectionItTest.java
+++ hibernate6-tests/hibernate6-tests-core/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/AbstractSQLInjectionItTest.java
@@ -45,16 +45,16 @@ public abstract class AbstractSQLInjectionItTest {
 
     private static Stream<Arguments> provideShouldNotModifyCurrentConfigurationPropertyWithExpectedValue() {
         return Stream.of(
-                Arguments.of("c.prop1"   ,   "some value"        , JSON_ALL_STATEMENT_PATTERN , "'top_element_with_set_of_values'" , "'TAG1', 'TAG2'" ,  new HashSet<>(Arrays.asList(1))),
-                Arguments.of("prop.value"   ,   "this is a test" , JSON_ALL_STATEMENT_PATTERN , "'top_element_with_set_of_values'" , "'TAG1', 'TAG2'" ,  new HashSet<>(Arrays.asList(1))),
-                Arguments.of("prop.value"   ,   "this is a test" , JSON_ANY_STATEMENT_PATTERN , "'top_element_with_set_of_values'" , "'TAG1', 'TAG2'" ,  new HashSet<>(Arrays.asList(1, 3)))
+                Arguments.of("c.prop1", "some value", JSON_ALL_STATEMENT_PATTERN, "'top_element_with_set_of_values'", "'TAG1', 'TAG2'", new HashSet<>(List.of(1))),
+                Arguments.of("prop.value", "this is a test", JSON_ALL_STATEMENT_PATTERN, "'top_element_with_set_of_values'", "'TAG1', 'TAG2'", new HashSet<>(List.of(1))),
+                Arguments.of("prop.value", "this is a test", JSON_ANY_STATEMENT_PATTERN, "'top_element_with_set_of_values'", "'TAG1', 'TAG2'", new HashSet<>(Arrays.asList(1, 3)))
         );
     }
 
     @DisplayName("should not modify current configuration property #property with expected value #value when executing correct statement, sql pattern #sqlPattern, jsonPathInput #jsonPathInput, array input #arrayInput")
     @ParameterizedTest
     @MethodSource("provideShouldNotModifyCurrentConfigurationPropertyWithExpectedValue")
-    public void shouldNotModifyCurrentConfigurationPropertyWithExpectedValue(String property, String value, String sqlPattern, String jsonPathInput, String arrayInput, Set<String> expectedQueryResults){
+    public void shouldNotModifyCurrentConfigurationPropertyWithExpectedValue(String property, String value, String sqlPattern, String jsonPathInput, String arrayInput, Set<String> expectedQueryResults) {
         //GIVEN
         jdbcTemplate.execute(String.format(SETTING_CONFIGURATION_PROPERTY_PATTERN, property, value));
         var query = String.format(sqlPattern, jsonPathInput, arrayInput);
@@ -67,5 +67,34 @@ public abstract class AbstractSQLInjectionItTest {
         var currentValue = jdbcTemplate.queryForObject(String.format(GETTING_CONFIGURATION_PROPERTY_PATTERN, property), String.class);
         assertThat(currentValue).isEqualTo(value);
         assertThat(new HashSet<>(results)).isEqualTo(expectedQueryResults);
+    }
+
+    private static Stream<Arguments> provideShouldModifyCurrentConfigurationPropertyAndChangeCurrentValueWithExpected() {
+        return Stream.of(
+                Arguments.of("c.prop1", "some value", JSON_ALL_STATEMENT_PATTERN, "'top_element_with_set_of_values'), array['X1']); SELECT set_config('c.prop1', 'SECURITY FAILED', false)--", "'TAG1', 'TAG2'", "SECURITY FAILED"),
+                Arguments.of("conf.value", "some value", JSON_ALL_STATEMENT_PATTERN, "'some_element'), array['sss']); SELECT set_config('conf.value', 'New value', false)--", "'Val1'", "New value"),
+                Arguments.of("prop.value", "this is a test", JSON_ALL_STATEMENT_PATTERN, "'top_element_with_set_of_values'", "'some val']); SELECT set_config('prop.value', 'WARNING', false);--", "WARNING"),
+                Arguments.of("prop.value", "this is a test", JSON_ANY_STATEMENT_PATTERN, "'top_element_with_set_of_values'", "'some val']); SELECT set_config('prop.value', 'WARNING', false);--", "WARNING"),
+                Arguments.of("conf.value", "some value", JSON_ANY_STATEMENT_PATTERN, "'some_element'), array['sss']); SELECT set_config('conf.value', 'New value', false)--", "'Val1'", "New value")
+        );
+    }
+
+    @DisplayName("should modify current configuration property #property and change current value #value with expected #expected when using statements jsonPath #jsonPath, array elements #array, sql statement #sqlPattern")
+    @ParameterizedTest
+    @MethodSource("provideShouldModifyCurrentConfigurationPropertyAndChangeCurrentValueWithExpected")
+    public void shouldModifyCurrentConfigurationPropertyAndChangeCurrentValueWithExpected(String property, String value, String sqlPattern, String jsonPath, String array, String expected) {
+        //GIVEN
+        jdbcTemplate.execute(String.format(SETTING_CONFIGURATION_PROPERTY_PATTERN, property, value));
+        var query = String.format(JSON_ALL_STATEMENT_PATTERN, jsonPath, array);
+        System.out.println("Testing query : " + query);
+
+        //WHEN
+        jdbcTemplate.execute(String.format(sqlPattern, jsonPath, array));
+
+        //THEN
+        var currentValue = jdbcTemplate.queryForObject(String.format(GETTING_CONFIGURATION_PROPERTY_PATTERN, property), String.class);
+        assertThat(currentValue).isEqualTo(expected);
+
+        // https://portswigger.net/web-security/sql-injection
     }
 }
