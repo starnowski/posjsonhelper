542604032290e6d9faaa3a8f1a9cb7b7767a2fce
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-06-19 20:44:52 +0200

    #58 - Added implementation [skip ci]

--- hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/descriptor/AbstractJsonbArrayStringsExistPredicateDescriptor.java
+++ hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/descriptor/AbstractJsonbArrayStringsExistPredicateDescriptor.java
@@ -2,16 +2,13 @@ package com.github.starnowski.posjsonhelper.hibernate6.descriptor;
 
 import com.github.starnowski.posjsonhelper.core.HibernateContext;
 import com.github.starnowski.posjsonhelper.hibernate6.JsonBExtractPath;
+import com.github.starnowski.posjsonhelper.hibernate6.operators.JsonArrayFunction;
 import com.github.starnowski.posjsonhelper.hibernate6.predicates.AbstractJsonbArrayStringsExistPredicate;
 import org.hibernate.query.ReturnableType;
 import org.hibernate.query.spi.QueryEngine;
 import org.hibernate.query.sqm.NodeBuilder;
-import org.hibernate.query.sqm.function.AbstractSqmSelfRenderingFunctionDescriptor;
 import org.hibernate.query.sqm.function.NamedSqmFunctionDescriptor;
 import org.hibernate.query.sqm.function.SelfRenderingSqmFunction;
-import org.hibernate.query.sqm.produce.function.ArgumentsValidator;
-import org.hibernate.query.sqm.produce.function.FunctionArgumentTypeResolver;
-import org.hibernate.query.sqm.produce.function.FunctionReturnTypeResolver;
 import org.hibernate.query.sqm.tree.SqmTypedNode;
 import org.hibernate.type.spi.TypeConfiguration;
 
@@ -20,18 +17,19 @@ import java.util.List;
 public abstract class AbstractJsonbArrayStringsExistPredicateDescriptor<T extends AbstractJsonbArrayStringsExistPredicate> extends NamedSqmFunctionDescriptor {
 
     protected final HibernateContext hibernateContext;
+
     public AbstractJsonbArrayStringsExistPredicateDescriptor(String name, HibernateContext hibernateContext) {
         super(name, false, null, null);
         this.hibernateContext = hibernateContext;
     }
 
-    abstract protected T generateJsonbArrayStringsExistPredicate(HibernateContext context, NodeBuilder nodeBuilder, JsonBExtractPath jsonBExtractPath, String[] values);
+    abstract protected T generateJsonbArrayStringsExistPredicate(HibernateContext context, NodeBuilder nodeBuilder, JsonBExtractPath jsonBExtractPath, JsonArrayFunction arrayFunction);
 
     abstract public String getSqmFunction();
 
     @Override
     protected <T> SelfRenderingSqmFunction<T> generateSqmFunctionExpression(List<? extends SqmTypedNode<?>> arguments, ReturnableType<T> impliedResultType, QueryEngine queryEngine, TypeConfiguration typeConfiguration) {
 
-        return generateJsonbArrayStringsExistPredicate(this.hibernateContext, (NodeBuilder) queryEngine.getCriteriaBuilder(), (JsonBExtractPath) arguments.get(0), null);
+        return generateJsonbArrayStringsExistPredicate(this.hibernateContext, queryEngine.getCriteriaBuilder(), (JsonBExtractPath) arguments.get(0), (JsonArrayFunction) arguments.get(1));
     }
 }
--- hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/descriptor/JsonbAllArrayStringsExistPredicateDescriptor.java
+++ hibernate6/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/descriptor/JsonbAllArrayStringsExistPredicateDescriptor.java
@@ -3,14 +3,10 @@ package com.github.starnowski.posjsonhelper.hibernate6.descriptor;
 import com.github.starnowski.posjsonhelper.core.Context;
 import com.github.starnowski.posjsonhelper.core.HibernateContext;
 import com.github.starnowski.posjsonhelper.hibernate6.JsonBExtractPath;
+import com.github.starnowski.posjsonhelper.hibernate6.operators.JsonArrayFunction;
 import com.github.starnowski.posjsonhelper.hibernate6.predicates.AbstractJsonbArrayStringsExistPredicate;
 import com.github.starnowski.posjsonhelper.hibernate6.predicates.JsonbAllArrayStringsExistPredicate;
 import org.hibernate.query.sqm.NodeBuilder;
-import org.hibernate.sql.ast.SqlAstTranslator;
-import org.hibernate.sql.ast.spi.SqlAppender;
-import org.hibernate.sql.ast.tree.SqlAstNode;
-
-import java.util.List;
 
 public class JsonbAllArrayStringsExistPredicateDescriptor extends AbstractJsonbArrayStringsExistPredicateDescriptor {
     public JsonbAllArrayStringsExistPredicateDescriptor(Context context, HibernateContext hibernateContext) {
@@ -18,8 +14,8 @@ public class JsonbAllArrayStringsExistPredicateDescriptor extends AbstractJsonbA
     }
 
     @Override
-    protected AbstractJsonbArrayStringsExistPredicate generateJsonbArrayStringsExistPredicate(HibernateContext context, NodeBuilder nodeBuilder, JsonBExtractPath jsonBExtractPath, String[] values) {
-        return new JsonbAllArrayStringsExistPredicate(context, nodeBuilder, jsonBExtractPath, values);
+    protected AbstractJsonbArrayStringsExistPredicate generateJsonbArrayStringsExistPredicate(HibernateContext context, NodeBuilder nodeBuilder, JsonBExtractPath jsonBExtractPath, JsonArrayFunction arrayFunction) {
+        return new JsonbAllArrayStringsExistPredicate(context, nodeBuilder, jsonBExtractPath, arrayFunction);
     }
 
     @Override
