a798775ef500b64e5b332551a91c43c47891b974
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-06-20 09:06:09 +0200

    #58 - Added tests [skip ci]

--- hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/descriptor/JsonArrayFunctionDescriptorTest.groovy
+++ hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/descriptor/JsonArrayFunctionDescriptorTest.groovy
@@ -1,5 +1,11 @@
 package com.github.starnowski.posjsonhelper.hibernate6.descriptor
 
+import com.github.starnowski.posjsonhelper.core.HibernateContext
+import org.hibernate.query.spi.QueryEngine
+import org.hibernate.query.sqm.function.SelfRenderingSqmFunction
+import org.hibernate.query.sqm.function.SqmFunctionRegistry
+import org.hibernate.query.sqm.internal.SqmCriteriaNodeBuilder
+import org.hibernate.query.sqm.tree.SqmTypedNode
 import org.hibernate.sql.ast.SqlAstNodeRenderingMode
 import org.hibernate.sql.ast.SqlAstTranslator
 import org.hibernate.sql.ast.spi.SqlAppender
@@ -28,5 +34,28 @@ class JsonArrayFunctionDescriptorTest extends Specification {
             2 * translator.render(_, SqlAstNodeRenderingMode.DEFAULT)
     }
 
+    def "should generate SqmFunctionExpression for method #method"() {
+        given:
+            QueryEngine queryEngine = Mock(QueryEngine)
+            SqmFunctionRegistry sqmFunctionRegistry = Mock(SqmFunctionRegistry)
+            List<? extends SqmTypedNode<?>> arguments = new ArrayList<>()
+            def criteriaBuilder = Mock(SqmCriteriaNodeBuilder)
+            def hibernateContext = Mock(HibernateContext)
+            JsonArrayFunctionDescriptor jsonArrayFunctionDescriptor = new JsonArrayFunctionDescriptor(hibernateContext)
+            hibernateContext.getJsonFunctionJsonArrayOperator() >> method
+
+        when:
+            def result = jsonArrayFunctionDescriptor.generateSqmFunctionExpression(arguments, null, queryEngine, null)
+
+        then:
+            queryEngine.getCriteriaBuilder() >> criteriaBuilder
+            criteriaBuilder.getQueryEngine() >> queryEngine
+            queryEngine.getSqmFunctionRegistry() >> sqmFunctionRegistry
+            sqmFunctionRegistry.findFunctionDescriptor(method) >> jsonArrayFunctionDescriptor
+            result instanceof SelfRenderingSqmFunction
+
+        where:
+            method << ["test_function", "arrayFunction"]
+    }
 
 }
