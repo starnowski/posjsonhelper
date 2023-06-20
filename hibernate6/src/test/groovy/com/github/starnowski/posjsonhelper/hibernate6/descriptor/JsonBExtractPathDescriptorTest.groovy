689e86ac707229f663cfd1753dc9aea8c5fd4568
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-06-20 09:33:37 +0200

    #58 - Added tests [skip ci]

---
+++ hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/descriptor/JsonBExtractPathDescriptorTest.groovy
@@ -0,0 +1,10 @@
+package com.github.starnowski.posjsonhelper.hibernate6.descriptor
+
+import spock.lang.Specification
+
+class JsonBExtractPathDescriptorTest extends AbstractJsonBExtractPathDescriptorTest<JsonBExtractPathDescriptor> {
+    @Override
+    protected JsonBExtractPathDescriptor generateTestedObject() {
+        new JsonBExtractPathDescriptor()
+    }
+}
