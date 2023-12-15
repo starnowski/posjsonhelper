ac92eea379f52042161a108ec8147bf6c5419cef
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-12-15 14:19:33 +0100

    #65 - Added tests case [skip ci]

--- core/src/test/groovy/com/github/starnowski/posjsonhelper/core/sql/SQLDefinitionContextFactorySupplierTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posjsonhelper/core/sql/SQLDefinitionContextFactorySupplierTest.groovy
@@ -1,8 +1,12 @@
 package com.github.starnowski.posjsonhelper.core.sql
 
+import com.github.starnowski.posjsonhelper.core.SystemPropertyReader
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import java.util.stream.Collectors
+
+import static com.github.starnowski.posjsonhelper.core.Constants.SQLDEFINITIONCONTEXTFACTORY_TYPES_PROPERTY
 
 class SQLDefinitionContextFactorySupplierTest extends Specification {
 
@@ -17,6 +21,25 @@ class SQLDefinitionContextFactorySupplierTest extends Specification {
             results.stream().map({it -> it.getClass()}).collect(Collectors.toSet()) == new HashSet([TestClasspathSQLDefinitionContextFactory1.class, TestClasspathSQLDefinitionContextFactory2.class])
     }
 
+    @Unroll
+    def "should return expected list of factories specified by system property value #value" (){
+        given:
+            def systemPropertyReader = Mock(SystemPropertyReader)
+            def tested = new SQLDefinitionContextFactorySupplier(null, systemPropertyReader)
 
-    //def factoriesSupplier = Mock(SQLDefinitionContextFactoryClasspathSupplier)
+        when:
+            def results = tested.get()
+
+        then:
+            1 * systemPropertyReader.read(SQLDEFINITIONCONTEXTFACTORY_TYPES_PROPERTY) >> value
+            results.stream().map({it -> it.getClass()}).collect(Collectors.toSet()) == expectedTypes
+
+        where:
+            value   || expectedTypes
+            "com.github.starnowski.posjsonhelper.core.sql.TestClasspathSQLDefinitionContextFactory1,com.github.starnowski.posjsonhelper.core.sql.TestClasspathSQLDefinitionContextFactory2"        ||  new HashSet([TestClasspathSQLDefinitionContextFactory1.class, TestClasspathSQLDefinitionContextFactory2.class])
+            "com.github.starnowski.posjsonhelper.core.sql.TestClasspathSQLDefinitionContextFactory2,com.github.starnowski.posjsonhelper.core.sql.TestClasspathSQLDefinitionContextFactory1"        ||  new HashSet([TestClasspathSQLDefinitionContextFactory1.class, TestClasspathSQLDefinitionContextFactory2.class])
+            "com.github.starnowski.posjsonhelper.core.sql.TestClasspathSQLDefinitionContextFactory1"        ||  new HashSet([TestClasspathSQLDefinitionContextFactory1.class])
+            "com.github.starnowski.posjsonhelper.core.sql.TestClasspathSQLDefinitionContextFactory2"        ||  new HashSet([TestClasspathSQLDefinitionContextFactory2.class])
+    }
+
 }
