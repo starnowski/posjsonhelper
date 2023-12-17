8f6799626627ad1181b1019b14ee61e1259c22df
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-12-17 21:24:28 +0100

    #68 - Added tests case [skip ci]

---
+++ hibernate6/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/descriptor/JsonBExtractPathTextDescriptorRegisterFactoryTest.groovy
@@ -0,0 +1,36 @@
+package com.github.starnowski.posjsonhelper.hibernate6.descriptor
+
+import com.github.starnowski.posjsonhelper.core.Context
+import com.github.starnowski.posjsonhelper.core.HibernateContext
+import org.hibernate.query.sqm.function.SqmFunctionRegistry
+import spock.lang.Specification
+
+class JsonBExtractPathTextDescriptorRegisterFactoryTest extends Specification {
+
+    def "should return correct function descriptor register"()
+    {
+        given:
+        def tested = new JsonBExtractPathTextDescriptorRegisterFactory()
+
+        when:
+        def result = tested.get(Context.builder().build(), HibernateContext.builder().build())
+
+        then:
+        result.class == AbstractJsonBExtractPathDescriptorRegister
+    }
+
+    def "function descriptor register should register correct function with key"()
+    {
+        given:
+        def tested = new JsonBExtractPathTextDescriptorRegisterFactory()
+        def functionRegister = tested.get(Context.builder().build(), HibernateContext.builder().build())
+        def sqmFunctionRegistry = new SqmFunctionRegistry()
+
+        when:
+        functionRegister.registerFunction(sqmFunctionRegistry)
+
+        then:
+        def result = sqmFunctionRegistry.findFunctionDescriptor("jsonb_extract_path_text")
+        result.class == JsonBExtractPathTextDescriptor
+    }
+}
