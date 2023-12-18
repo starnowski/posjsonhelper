379c9f82acbe441df1101a7ad49b4a38819eda07
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-12-18 22:03:24 +0100

    #67 - Added dao (not yet finished!) [skip ci]

--- text/hibernate6-text/src/test/java/com/github/starnowski/posjsonhelper/text/hibernate6/dao/TweetDao.java
+++ text/hibernate6-text/src/test/java/com/github/starnowski/posjsonhelper/text/hibernate6/dao/TweetDao.java
@@ -1,4 +1,18 @@
 package com.github.starnowski.posjsonhelper.text.hibernate6.dao;
 
+import com.github.starnowski.posjsonhelper.text.hibernate6.model.Tweet;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class TweetDao {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public List<Tweet> findBySinglePhraseInDescriptionForDefaultConfiguration(String phrase)
+    {
+        //TODO
+        return null;
+    }
 }
