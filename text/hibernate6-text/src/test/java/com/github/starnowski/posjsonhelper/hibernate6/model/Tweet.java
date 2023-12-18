dcc27b39f1920941ba4824249154ff867499c690
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-12-18 10:55:51 +0100

    #67 - Added tests model [skip ci]

---
+++ text/hibernate6-text/src/test/java/com/github/starnowski/posjsonhelper/hibernate6/model/Tweet.java
@@ -0,0 +1,42 @@
+package com.github.starnowski.posjsonhelper.hibernate6.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tweet")
+public class Tweet {
+
+    @Id
+    private Long id;
+    @Column
+    private String title;
+    @Column(name = "short_content")
+    private String shortContent;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortContent() {
+        return shortContent;
+    }
+
+    public void setShortContent(String shortContent) {
+        this.shortContent = shortContent;
+    }
+}
