41ac86c0638e092d6d2424fb9b4bf47d3e39fa05
starnowski <33316705+starnowski@users.noreply.github.com>
2023-12-21 23:48:52 +0100

    #30 - Added scripts for docker

--- scripts/prepareDatabase.sh
+++ scripts/prepareDatabase.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 DIRNAME="$(dirname $0)"
 POSTGRES_HOST=127.0.0.1
+DATABASE_PORT=5432
 
 # Call getopt to validate the provided input.
-options=$(getopt -o "" --long postgres_host: -- "$@")
+options=$(getopt -o "" --long postgres_host:,postgres_port: -- "$@")
 [ $? -eq 0 ] || {
     echo "Incorrect options provided"
     exit 1
@@ -15,6 +16,10 @@ while true; do
         shift; # The arg is next in position args
         export POSTGRES_HOST="$1"
         ;;
+    --postgres_port)
+        shift; # The arg is next in position args
+        export DATABASE_PORT="$1"
+        ;;
     --)
         shift
         break
@@ -24,6 +29,6 @@ while true; do
 done
 
 export PGPASSWORD=postgres
-psql -f "${DIRNAME}/../test-utils/src/main/resources/create-database-owner.sql" -U postgres -h "${POSTGRES_HOST}"
-psql -f "${DIRNAME}/../test-utils/src/main/resources/create-database.sql" -U postgres -h "${POSTGRES_HOST}"
-psql -f "${DIRNAME}/../test-utils/src/main/resources/create-database-core-database-and-data.sql" -U postgres -h "${POSTGRES_HOST}"
+psql -f "${DIRNAME}/../test-utils/src/main/resources/create-database-owner.sql" -U postgres -h "${POSTGRES_HOST}" -p "${DATABASE_PORT}"
+psql -f "${DIRNAME}/../test-utils/src/main/resources/create-database.sql" -U postgres -h "${POSTGRES_HOST}" -p "${DATABASE_PORT}"
+psql -f "${DIRNAME}/../test-utils/src/main/resources/create-database-core-database-and-data.sql" -U postgres -h "${POSTGRES_HOST}" -p "${DATABASE_PORT}"
