92f511b6f96809bf511191c60393006faae98717
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-12-30 22:21:52 +0100

    #84 - Added tests data [skip ci]

--- text/hibernate6-text-tests/src/test/resources/com/github/starnowski/posjsonhelper/text/hibernate6/dao/tweets-with-locale.sql
+++ text/hibernate6-text-tests/src/test/resources/com/github/starnowski/posjsonhelper/text/hibernate6/dao/tweets-with-locale.sql
@@ -4,5 +4,8 @@ INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (2, 'Hyd
 INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (3, 'Hydrogen batteries', 'It is great idea for storing electicity power', 'english');
 
 INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (4, 'Translation', 'Wyraz "Hydrogen" znaczy wodór w języku angielskim', 'polish');
-INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (5, 'Buty narciarskie', 'Postgres is one of the widly used database on the market', 'polish');
-INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (6, 'Database', 'On the market there is a lot of database that have similar features like Oracle', 'polish');
+
+INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (5, 'Zmywarka Electrolux ESF2400OK 55cm', 'Wymiary (SxWxG): 55 x 43,8 x 50 cm. Zużycie prądu (100 cykli): 61 kWh', 'polish');
+INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (6, 'Zmywarka Bosch Serie 2 SKS51E32EU 55,1cm', 'Wymiary (SxWxG): 55,1 x 45 x 50 cm. Zużycie prądu (100 cykli): 61 kWh', 'polish');
+INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (7, 'Zmywarka Toshiba DW-08T2(S) 55cm', 'Wymiary (SxWxG): 55 x 59 x 50 cm. Zużycie prądu (100 cykli): 72 kWh', 'polish');
+INSERT INTO tweet_with_locale (id, title, short_content, locale) VALUES (8, 'Zmywarka Siemens iQ300 SK26E822EU 55,1cm', 'Wymiary (SxWxG): 55,1 x 45 x 50 cm. Zużycie prądu (100 cykli): 61 kWh', 'polish');
