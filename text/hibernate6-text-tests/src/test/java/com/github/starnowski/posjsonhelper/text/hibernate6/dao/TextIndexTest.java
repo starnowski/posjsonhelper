ba13cd59befb464d4bdca17dfb63ba456fbd7683
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-12-31 13:59:28 +0100

    #84 - Added tests [skip ci]

--- text/hibernate6-text-tests/src/test/java/com/github/starnowski/posjsonhelper/text/hibernate6/dao/TextIndexTest.java
+++ text/hibernate6-text-tests/src/test/java/com/github/starnowski/posjsonhelper/text/hibernate6/dao/TextIndexTest.java
@@ -1,21 +1,15 @@
 package com.github.starnowski.posjsonhelper.text.hibernate6.dao;
 
-import com.github.starnowski.posjsonhelper.text.hibernate6.model.Tweet;
 import com.github.starnowski.posjsonhelper.text.hibernate6.model.TweetWithLocale;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -24,7 +18,6 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
@@ -43,13 +36,17 @@ public class TextIndexTest extends AbstractItTest {
 
     private static Stream<Arguments> provideShouldFindCorrectTweetsBySinglePlainQueryInDescription() {
         return Stream.of(
-                Arguments.of("EV future", ENGLISH_CONFIGURATION, asList(1L)),
+                Arguments.of("EV future", ENGLISH_CONFIGURATION, List.of(1L)),
                 Arguments.of("Hydrogen", ENGLISH_CONFIGURATION, asList(2L, 3L)),
                 Arguments.of("Hydrogen", POLISH_CONFIGURATION, emptyList()),
-                Arguments.of("Wodór", POLISH_CONFIGURATION, asList(4L)),
-                Arguments.of("Zmywarka czarny", POLISH_CONFIGURATION, asList(5L)),
+                Arguments.of("Wodór", POLISH_CONFIGURATION, List.of(4L)),
+                Arguments.of("Zmywarka czarny", POLISH_CONFIGURATION, List.of(5L)),
                 Arguments.of("Zmywarka biały", POLISH_CONFIGURATION, asList(6L, 7L)),
-                Arguments.of("Zmywarka srebrny", POLISH_CONFIGURATION, asList(8L))
+                Arguments.of("Zmywarka srebrny", POLISH_CONFIGURATION, List.of(8L)),
+                Arguments.of("Electrolux", POLISH_CONFIGURATION, List.of(5L)),
+                Arguments.of("Bosch", POLISH_CONFIGURATION, List.of(6L)),
+                Arguments.of("Toshiba", POLISH_CONFIGURATION, List.of(7L)),
+                Arguments.of("Siemens", POLISH_CONFIGURATION, List.of(8L))
         );
     }
 
