a95060305ef8016156dc48186bb78501501ecd03
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-01-02 20:08:25 +0100

    #101 - Added javadoc for the CastOperatorFunction component [skip ci]

--- hibernate6-core/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/operators/CastOperatorFunction.java
+++ hibernate6-core/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/operators/CastOperatorFunction.java
@@ -1,23 +1,23 @@
 /**
- *     Posjsonhelper library is an open-source project that adds support of
- *     Hibernate query for https://www.postgresql.org/docs/10/functions-json.html)
- *
- *     Copyright (C) 2023  Szymon Tarnowski
- *
- *     This library is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU Lesser General Public
- *     License as published by the Free Software Foundation; either
- *     version 2.1 of the License, or (at your option) any later version.
- *
- *     This library is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *     Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with this library; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- *     USA
+ * Posjsonhelper library is an open-source project that adds support of
+ * Hibernate query for https://www.postgresql.org/docs/10/functions-json.html)
+ * <p>
+ * Copyright (C) 2023  Szymon Tarnowski
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 package com.github.starnowski.posjsonhelper.hibernate6.operators;
 
@@ -39,10 +39,22 @@ public class CastOperatorFunction extends SelfRenderingSqmFunction<String> imple
     private final SqmExpression<String> type;
     private final SqmExpression<?> argument;
 
+    /**
+     * @param nodeBuilder      component of type {@link NodeBuilder}
+     * @param argument         value for cast operator
+     * @param type             type to which values should be cast
+     * @param hibernateContext context object of type {@link HibernateContext}
+     */
     public CastOperatorFunction(NodeBuilder nodeBuilder, SqmExpression<?> argument, String type, HibernateContext hibernateContext) {
         this(nodeBuilder, argument, nodeBuilder.literal(type), hibernateContext);
     }
 
+    /**
+     * @param nodeBuilder      component of type {@link NodeBuilder}
+     * @param argument         value for cast operator
+     * @param type             type to which values should be cast
+     * @param hibernateContext context object of type {@link HibernateContext}
+     */
     public CastOperatorFunction(NodeBuilder nodeBuilder, SqmExpression<?> argument, SqmExpression<String> type, HibernateContext hibernateContext) {
         super(nodeBuilder.getQueryEngine().getSqmFunctionRegistry().findFunctionDescriptor(hibernateContext.getCastFunctionOperator()),
                 (FunctionRenderer) nodeBuilder.getQueryEngine().getSqmFunctionRegistry().findFunctionDescriptor(hibernateContext.getCastFunctionOperator()),
