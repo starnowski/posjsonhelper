bfa5455d028861b74d3f72cc0606ac1f232ac9f6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-01-03 07:43:05 +0100

    #101 - Added javadoc for the TSVectorFunctionDescriptor component [skip ci]

--- text/hibernate6-text/src/main/java/com/github/starnowski/posjsonhelper/text/hibernate6/descriptor/TSVectorFunctionDescriptor.java
+++ text/hibernate6-text/src/main/java/com/github/starnowski/posjsonhelper/text/hibernate6/descriptor/TSVectorFunctionDescriptor.java
@@ -1,23 +1,23 @@
 /**
- *     Posjsonhelper library is an open-source project that adds support of
- *     Hibernate query for https://www.postgresql.org/docs/10/functions-json.html)
- *
- *     Copyright (C) 2023  Szymon Tarnowski
- *
- *     This library is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU Lesser General Public
- *     License as published by the Free Software Foundation; either
- *     version 2.1 of the License, or (at your option) any later version.
- *
- *     This library is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *     Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with this library; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- *     USA
+ * Posjsonhelper library is an open-source project that adds support of
+ * Hibernate query for https://www.postgresql.org/docs/10/functions-json.html)
+ * <p>
+ * Copyright (C) 2023  Szymon Tarnowski
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 package com.github.starnowski.posjsonhelper.text.hibernate6.descriptor;
 
@@ -29,12 +29,14 @@ import org.hibernate.query.sqm.function.NamedSqmFunctionDescriptor;
 import org.hibernate.query.sqm.function.SelfRenderingSqmFunction;
 import org.hibernate.query.sqm.tree.SqmTypedNode;
 import org.hibernate.query.sqm.tree.expression.SqmExpression;
-import org.hibernate.type.spi.TypeConfiguration;
 
 import java.util.List;
 
 import static com.github.starnowski.posjsonhelper.core.Constants.TO_TSVECTOR_FUNCTION_NAME;
 
+/**
+ * Function descriptor for child type of {@link TSVectorFunction}
+ */
 public class TSVectorFunctionDescriptor extends NamedSqmFunctionDescriptor {
     public TSVectorFunctionDescriptor() {
         super(TO_TSVECTOR_FUNCTION_NAME, false, null, null);
@@ -43,6 +45,6 @@ public class TSVectorFunctionDescriptor extends NamedSqmFunctionDescriptor {
     @Override
     protected <T> SelfRenderingSqmFunction<T> generateSqmFunctionExpression(List<? extends SqmTypedNode<?>> arguments, ReturnableType<T> impliedResultType, QueryEngine queryEngine) {
         //TODO Check arguments size
-        return (SelfRenderingSqmFunction<T>) new TSVectorFunction((Path) arguments.get(arguments.size() > 1 ? 1: 0), arguments.size() > 1 ? (SqmExpression<?>) arguments.get(0) : null,queryEngine.getCriteriaBuilder());
+        return (SelfRenderingSqmFunction<T>) new TSVectorFunction((Path) arguments.get(arguments.size() > 1 ? 1 : 0), arguments.size() > 1 ? (SqmExpression<?>) arguments.get(0) : null, queryEngine.getCriteriaBuilder());
     }
 }
