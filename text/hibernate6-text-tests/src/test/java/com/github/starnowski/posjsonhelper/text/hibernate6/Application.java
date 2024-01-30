2219532f3c3900a1d3148d05ecdefd1fb0ce9f40
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-01-30 07:25:23 +0100

    #109 - Added tests data [skip ci]

--- text/hibernate6-text-tests/src/test/java/com/github/starnowski/posjsonhelper/text/hibernate6/Application.java
+++ text/hibernate6-text-tests/src/test/java/com/github/starnowski/posjsonhelper/text/hibernate6/Application.java
@@ -14,6 +14,7 @@ public class Application {
     public static final String TEXT_INDEX_SCRIPT_PATH = "text-index.sql";
     public static final String ENGLISH_CONFIGURATION = "english";
     public static final String POLISH_CONFIGURATION = "pl_ispell";
+    public static final String POLISH_EXTENDED_CONFIGURATION = "pl_ex_ispell";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
