fa505f665faff2bdddc3ba117ce8bce43174c2e2
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-03-10 10:15:30 +0100

    #116 - Added unit test for super methods.

--- hibernate6-core/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/descriptor/NamedSqmFunctionWithSchemaReferenceDescriptorTest.groovy
+++ hibernate6-core/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/descriptor/NamedSqmFunctionWithSchemaReferenceDescriptorTest.groovy
@@ -170,4 +170,57 @@ class NamedSqmFunctionWithSchemaReferenceDescriptorTest extends Specification {
             "non_public_schema"     | ["fun", "get_some"]                       |   "fun"
     }
 
+    @Unroll
+    def "render method with predicate and two boolean parameters for function (#funName) without adding schema reference (#schema) at the beginning of statement (#functionThatRequiredExecutionWithSchema)"() {
+        given:
+            Set<String> functionThatRequiredExecutionWithSchemaSet = new HashSet<>(ofNullable(functionThatRequiredExecutionWithSchema).orElse(new ArrayList()))
+            def descriptor = new NamedSqmFunctionWithSchemaReferenceDescriptor(funName, Context.builder()
+                    .withSchema(schema)
+                    .withFunctionsThatShouldBeExecutedWithSchemaReference(functionThatRequiredExecutionWithSchemaSet).build(),
+                    HibernateContext.builder().build())
+            def sqlAppender = new StringBuilderSqlAppender()
+            SqlAstTranslator sqlAstTranslator = Mock(SqlAstTranslator)
+            sqlAstTranslator.getCurrentClauseStack() >> Mock(org.hibernate.internal.util.collections.Stack)
+
+        when:
+            descriptor.render(sqlAppender, [], (Predicate) null, null, null, (ReturnableType)null, sqlAstTranslator)
+
+        then:
+            !sqlAppender.toString().startsWith(schema + ".")
+
+        where:
+            schema                  | functionThatRequiredExecutionWithSchema   |   funName
+            "sche1"                 | []                                        |   "funcName"
+            "non_public_schema"     | null                                      |   "funcName"
+            "non_public_schema"     | []                                        |   "fun1"
+            "non_public_schema"     | ["some function"]                         |   "some_other_fun"
+            "non_public_schema"     | ["fun", "get_some"]                       |   "some_other_fun"
+    }
+
+    @Unroll
+    def "render method with predicate and two boolean parameters for function (#funName) with adding schema reference (#schema) at the beginning of statement (#functionThatRequiredExecutionWithSchema)"() {
+        given:
+            Set<String> functionThatRequiredExecutionWithSchemaSet = new HashSet<>(ofNullable(functionThatRequiredExecutionWithSchema).orElse(new ArrayList()))
+            def descriptor = new NamedSqmFunctionWithSchemaReferenceDescriptor(funName, Context.builder()
+                    .withSchema(schema)
+                    .withFunctionsThatShouldBeExecutedWithSchemaReference(functionThatRequiredExecutionWithSchemaSet).build(),
+                    HibernateContext.builder().build())
+            def sqlAppender = new StringBuilderSqlAppender()
+            SqlAstTranslator sqlAstTranslator = Mock(SqlAstTranslator)
+            sqlAstTranslator.getCurrentClauseStack() >> Mock(org.hibernate.internal.util.collections.Stack)
+
+        when:
+            descriptor.render(sqlAppender, [], (Predicate) null, null, null, (ReturnableType)null, sqlAstTranslator)
+
+        then:
+            sqlAppender.toString().startsWith(schema + ".")
+
+        where:
+            schema                  | functionThatRequiredExecutionWithSchema   |   funName
+            "sche1"                 | ["funcName"]                              |   "funcName"
+            "non_public_schema"     | ["get_some", "fun1"]                      |   "fun1"
+            "non_public_schema"     | ["some_other_fun"]                        |   "some_other_fun"
+            "non_public_schema"     | ["fun", "get_some"]                       |   "fun"
+    }
+
 }
