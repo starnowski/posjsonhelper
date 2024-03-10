e4af119a68256ade9b5b74aab7b399f5355bfe46
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-03-10 10:07:07 +0100

    #116 - Added unit test for super methods. [skip ci]

--- hibernate6-core/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/descriptor/NamedSqmFunctionWithSchemaReferenceDescriptorTest.groovy
+++ hibernate6-core/src/test/groovy/com/github/starnowski/posjsonhelper/hibernate6/descriptor/NamedSqmFunctionWithSchemaReferenceDescriptorTest.groovy
@@ -3,9 +3,11 @@ package com.github.starnowski.posjsonhelper.hibernate6.descriptor
 import com.github.starnowski.posjsonhelper.core.Context
 import com.github.starnowski.posjsonhelper.core.HibernateContext
 import org.hibernate.query.ReturnableType
+import org.hibernate.query.SortDirection
 import org.hibernate.sql.ast.SqlAstTranslator
-import org.hibernate.sql.ast.spi.SqlAppender
 import org.hibernate.sql.ast.spi.StringBuilderSqlAppender
+import org.hibernate.sql.ast.tree.expression.Expression
+import org.hibernate.sql.ast.tree.predicate.Predicate
 import org.hibernate.sql.ast.tree.select.SortSpecification
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -60,6 +62,33 @@ class NamedSqmFunctionWithSchemaReferenceDescriptorTest extends Specification {
             "non_public_schema"     | ["get_some", "fun1"]                      |   "fun1"
             "non_public_schema"     | ["some_other_fun"]                        |   "some_other_fun"
             "non_public_schema"     | ["fun", "get_some"]                       |   "fun"
+    }
+
+    @Unroll
+    def "render method with predicate and sort specification for function (#funName) without adding schema reference (#schema) at the beginning of statement (#functionThatRequiredExecutionWithSchema)"() {
+        given:
+            Set<String> functionThatRequiredExecutionWithSchemaSet = new HashSet<>(ofNullable(functionThatRequiredExecutionWithSchema).orElse(new ArrayList()))
+            def descriptor = new NamedSqmFunctionWithSchemaReferenceDescriptor(funName, Context.builder()
+                    .withSchema(schema)
+                    .withFunctionsThatShouldBeExecutedWithSchemaReference(functionThatRequiredExecutionWithSchemaSet).build(),
+                    HibernateContext.builder().build())
+            def sqlAppender = new StringBuilderSqlAppender()
+            SqlAstTranslator sqlAstTranslator = Mock(SqlAstTranslator)
+            sqlAstTranslator.getCurrentClauseStack() >> Mock(org.hibernate.internal.util.collections.Stack)
+
+        when:
+            descriptor.render(sqlAppender, [], (Predicate) null, [new SortSpecification(Mock(Expression), SortDirection.ASCENDING)], (ReturnableType)null, sqlAstTranslator)
+
+        then:
+            !sqlAppender.toString().startsWith(schema + ".")
+
+        where:
+            schema                  | functionThatRequiredExecutionWithSchema   |   funName
+            "sche1"                 | []                                        |   "funcName"
+            "non_public_schema"     | null                                      |   "funcName"
+            "non_public_schema"     | []                                        |   "fun1"
+            "non_public_schema"     | ["some function"]                         |   "some_other_fun"
+            "non_public_schema"     | ["fun", "get_some"]                       |   "some_other_fun"
     }
 //
 //    def "render method with filter parameter"() {
