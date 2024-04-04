2494d8f1a627ef72f0acaf77d5da53cd65268e2b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-04-05 00:37:51 +0200

    #120 - Added implementation for JsonbSetFunctionJsonPathBuilder type [skip ci]

--- json-core/src/main/java/com/github/starnowski/posjsonhelper/json/core/sql/JsonbSetFunctionJsonPathBuilder.java
+++ json-core/src/main/java/com/github/starnowski/posjsonhelper/json/core/sql/JsonbSetFunctionJsonPathBuilder.java
@@ -2,19 +2,18 @@ package com.github.starnowski.posjsonhelper.json.core.sql;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JsonbSetFunctionJsonPathBuilder {
 
-    private List<Object> path = new ArrayList<>();
+    private final List<Object> path = new ArrayList<>();
 
     public JsonbSetFunctionJsonPathBuilder append(Object node) {
         path.add(node);
         return this;
     }
 
-    public String build()
-    {
-        //TODO
-        return null;
+    public String build() {
+        return "{" + path.stream().map(String::valueOf).collect(Collectors.joining(",")) + "}";
     }
 }
