4361a1ff0b7eb51ccce6c306a5c475dc8855a69b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-04-10 00:23:57 +0200

    #120 - Added JsonUpdateStatementConfigurationBuilder (not yet finished!) [skip ci]

--- json-core/src/main/java/com/github/starnowski/posjsonhelper/json/core/sql/JsonUpdateStatementConfigurationBuilder.java
+++ json-core/src/main/java/com/github/starnowski/posjsonhelper/json/core/sql/JsonUpdateStatementConfigurationBuilder.java
@@ -5,11 +5,37 @@ import java.util.List;
 
 public class JsonUpdateStatementConfigurationBuilder {
 
-    private List<JsonUpdateStatementConfiguration.JsonUpdateStatementOperation> operations = new ArrayList<>();
+    private final List<JsonUpdateStatementConfiguration.JsonUpdateStatementOperation> operations = new ArrayList<>();
+    private JsonUpdateStatementOperationSort sort;
+    private JsonUpdateStatementOperationFilter postSortFilter;
 
-    public JsonUpdateStatementConfigurationBuilder append(JsonUpdateStatementOperationType operation, JsonTextArray jsonTextArray, String value){
-        //TODO
+    public JsonUpdateStatementConfigurationBuilder withSort(JsonUpdateStatementOperationSort sort) {
+        this.sort = sort;
+        return this;
+    }
+
+    public JsonUpdateStatementConfigurationBuilder withPostSortFilter(JsonUpdateStatementOperationFilter postSortFilter) {
+        this.postSortFilter = postSortFilter;
+        return this;
+    }
+
+    public JsonUpdateStatementConfigurationBuilder append(JsonUpdateStatementOperationType operation, JsonTextArray jsonTextArray, String value) {
         operations.add(new JsonUpdateStatementConfiguration.JsonUpdateStatementOperation(jsonTextArray, operation, value));
         return this;
+    }
+
+    public JsonUpdateStatementConfiguration build() {
+        //TODO
+        return new JsonUpdateStatementConfiguration(operations);
+    }
+
+    public interface JsonUpdateStatementOperationSort {
+
+        List<JsonUpdateStatementConfiguration.JsonUpdateStatementOperation> sort(List<JsonUpdateStatementConfiguration.JsonUpdateStatementOperation> operations);
+    }
+
+    public interface JsonUpdateStatementOperationFilter {
+
+        List<JsonUpdateStatementConfiguration.JsonUpdateStatementOperation> filter(List<JsonUpdateStatementConfiguration.JsonUpdateStatementOperation> operations);
     }
 }
---
+++ json-core/src/test/groovy/com/github/starnowski/posjsonhelper/json/core/sql/JsonUpdateStatementConfigurationBuilderTest.groovy
@@ -0,0 +1,8 @@
+package com.github.starnowski.posjsonhelper.json.core.sql
+
+import spock.lang.Specification
+
+class JsonUpdateStatementConfigurationBuilderTest extends Specification {
+
+    //TODO
+}
