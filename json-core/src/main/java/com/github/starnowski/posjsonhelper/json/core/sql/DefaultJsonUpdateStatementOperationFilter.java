12f51233ea2ca9911c3f3fc37c68e0fffc25edcc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-04-11 21:04:17 +0200

    #120 - Added implementation for filtering (not yet finished!) [skip ci]

--- json-core/src/main/java/com/github/starnowski/posjsonhelper/json/core/sql/DefaultJsonUpdateStatementOperationFilter.java
+++ json-core/src/main/java/com/github/starnowski/posjsonhelper/json/core/sql/DefaultJsonUpdateStatementOperationFilter.java
@@ -1,6 +1,9 @@
 package com.github.starnowski.posjsonhelper.json.core.sql;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class DefaultJsonUpdateStatementOperationFilter implements JsonUpdateStatementConfigurationBuilder.JsonUpdateStatementOperationFilter {
     @Override
@@ -8,5 +11,40 @@ public class DefaultJsonUpdateStatementOperationFilter implements JsonUpdateStat
         // Process/Gather information about operations objects
         // Filter operations objects based on context
         return operations;
+    }
+
+    private OperationFilterContext buildOperationFilterContext(List<JsonUpdateStatementConfiguration.JsonUpdateStatementOperation> operations){
+        OperationFilterContext context = new OperationFilterContext();
+
+        return context;
+    }
+
+    private class OperationFilterContext {
+
+        Map<JsonTextArrayJsonUpdateStatementOperationTypeKey, Integer> numberOfOperations = new HashMap<>();
+        Map<JsonTextArrayJsonUpdateStatementOperationTypeKey, Integer> lastIndexOperations = new HashMap<>();
+    }
+
+    private class JsonTextArrayJsonUpdateStatementOperationTypeKey {
+        private final JsonTextArray jsonTextArray;
+        private final JsonUpdateStatementOperationType type;
+
+        public JsonTextArrayJsonUpdateStatementOperationTypeKey(JsonTextArray jsonTextArray, JsonUpdateStatementOperationType type) {
+            this.jsonTextArray = jsonTextArray;
+            this.type = type;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            JsonTextArrayJsonUpdateStatementOperationTypeKey that = (JsonTextArrayJsonUpdateStatementOperationTypeKey) o;
+            return Objects.equals(jsonTextArray, that.jsonTextArray) && type == that.type;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(jsonTextArray, type);
+        }
     }
 }
