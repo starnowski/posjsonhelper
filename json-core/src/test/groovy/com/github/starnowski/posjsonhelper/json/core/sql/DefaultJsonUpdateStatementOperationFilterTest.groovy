dd2e079251781a4adafed45e740d1e6d4dd1c9de
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-04-15 08:42:01 +0200

    #120 - Added tests case

--- json-core/src/test/groovy/com/github/starnowski/posjsonhelper/json/core/sql/DefaultJsonUpdateStatementOperationFilterTest.groovy
+++ json-core/src/test/groovy/com/github/starnowski/posjsonhelper/json/core/sql/DefaultJsonUpdateStatementOperationFilterTest.groovy
@@ -6,7 +6,7 @@ import spock.lang.Unroll
 class DefaultJsonUpdateStatementOperationFilterTest extends Specification {
 
     @Unroll
-    def "should filter correctly json path (current #operations) (expected #expected) : #message"() {
+    def "should filter correctly operations (current #operations) (expected #expected) : #message"() {
         given:
             def tested = new DefaultJsonUpdateStatementOperationFilter()
 
@@ -19,6 +19,21 @@ class DefaultJsonUpdateStatementOperationFilterTest extends Specification {
         where:
         operations  ||  expected    |   message
         [jsonbSet((new JsonTextArrayBuilder()).append("a").build(), "b1"), jsonbSet((new JsonTextArrayBuilder()).append("a").build(), "c1")] || [jsonbSet((new JsonTextArrayBuilder()).append("a").build(), "c1")] | "filter redundant operation that is going to override by next operation"
+    }
+
+    @Unroll
+    def "should not filter when there is nothing to filter for operations (current #operations)"() {
+        given:
+            def tested = new DefaultJsonUpdateStatementOperationFilter()
+
+        when:
+            def result = tested.filter(operations)
+
+        then:
+            result == operations
+
+        where:
+            operations << [[jsonbSet((new JsonTextArrayBuilder()).append("a").build(), "c1")]]
     }
 
     private static JsonUpdateStatementConfiguration.JsonUpdateStatementOperation jsonbSet(JsonTextArray jsonTextArray, String json)
