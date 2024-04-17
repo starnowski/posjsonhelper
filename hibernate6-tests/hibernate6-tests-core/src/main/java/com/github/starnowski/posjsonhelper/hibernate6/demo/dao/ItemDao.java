cccd6aa5041cb1c7aad171110d779bdf5930581b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-04-17 09:21:30 +0200

    #120 - Removed TODO comment [skip ci]

--- hibernate6-tests/hibernate6-tests-core/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/ItemDao.java
+++ hibernate6-tests/hibernate6-tests-core/src/main/java/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/ItemDao.java
@@ -134,8 +134,6 @@ public class ItemDao {
         //top_element_with_set_of_values
         String statement = String.format("from Item as item_ where NOT ( %s( jsonb_extract_path( item_.jsonbContent , :param0 ) , %s(%s)) = TRUE ) OR jsonb_extract_path( item_.jsonbContent , :param0 ) IS NULL ", hibernateContext.getJsonbAllArrayStringsExistOperator(), hibernateContext.getJsonFunctionJsonArrayOperator(), generateParameters("param", 1, tags.size()));
         TypedQuery<Item> query = entityManager.createQuery(statement, Item.class);
-//        query.setParameter("path", "string_value");
-//        query.setParameter("expr", expression);
         query.setParameter("param0", "top_element_with_set_of_values");
         List<String> parameters = tags.stream().toList();
         for (int p = 1, i = 0; p < parameters.size() + 1; p++, i++) {
@@ -145,7 +143,6 @@ public class ItemDao {
     }
 
     public List<Item> findAllByAnyMatchingTagsInInnerElement(HashSet<String> tags) {
-        //TODO Add tests
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Item> query = cb.createQuery(Item.class);
         Root<Item> root = query.from(Item.class);
