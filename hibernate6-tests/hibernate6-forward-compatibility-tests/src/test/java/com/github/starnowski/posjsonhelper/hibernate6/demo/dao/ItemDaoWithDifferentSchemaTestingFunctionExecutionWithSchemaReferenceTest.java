43434950cd6c4d23c254f593095126d29db93cf8
starnowski <33316705+starnowski@users.noreply.github.com>
2024-11-19 00:08:56 +0100

    #145 Temporary disabled tests cases for hibernate version 6.6.2

--- hibernate6-tests/hibernate6-forward-compatibility-tests/src/test/java/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/ItemDaoWithDifferentSchemaTestingFunctionExecutionWithSchemaReferenceTest.java
+++ hibernate6-tests/hibernate6-forward-compatibility-tests/src/test/java/com/github/starnowski/posjsonhelper/hibernate6/demo/dao/ItemDaoWithDifferentSchemaTestingFunctionExecutionWithSchemaReferenceTest.java
@@ -1,8 +1,10 @@
 package com.github.starnowski.posjsonhelper.hibernate6.demo.dao;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@Disabled("There are some problems with schema reference https://github.com/starnowski/posjsonhelper/issues/153")
 @ActiveProfiles("different-schema-testing-function-execution-with-schema-reference")
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.datasource.url= jdbc:postgresql://localhost:${posjsonhelper.test.database.port:5432}/posjsonhelper_db"})
 public class ItemDaoWithDifferentSchemaTestingFunctionExecutionWithSchemaReferenceTest extends AbstractItemDaoTest {
